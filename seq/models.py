42ce71d3662019a24382014a5fb2e462ff1c7a68
Ali Ebrahim <aebrahim@ucsd.edu>
2013-03-18 14:42:00 -0700

    add more information when uploading breseq data

--- seq/models.py
+++ seq/models.py
@@ -8,6 +8,21 @@ class ResequencingExperiment(models.Model):
     reads = models.IntegerField(blank=True, null=True)
     average_read_length = models.FloatField(blank=True, null=True)
     mutations = models.ManyToManyField("Mutation", through="ObservedMutation")
+    location = models.CharField(max_length=200, blank=True, null=True)
+    
+    @property
+    def mean_coverage(self):
+        return self.average_read_length * self.reads / 4639675.
+    
+    @property
+    def ale_id(self):
+        return self.isolate.flask.ale_id.ale_id
+    @property
+    def flask_number(self):
+        return self.isolate.flask.flask_number
+    
+    def get_isolate_name(self):
+        return u"A%d_F%d_I%d" % (self.ale_id, self.flask_number, self.isolate.isolate_number)
     # TODO - add more information
 
 class Mutation(models.Model):
@@ -15,6 +30,8 @@ class Mutation(models.Model):
     position = models.IntegerField()
     feature_length = models.IntegerField(blank=True, null=True)
     sequence_change = models.CharField(max_length=100)
+    protein_change = models.CharField(max_length=300, blank=True, null=True)
+    gene = models.CharField(max_length=300, blank=True, null=True)
     class Meta:
         unique_together = (("position", "sequence_change"),)
 
@@ -22,3 +39,4 @@ class ObservedMutation(models.Model):
     sequencing_experiment = models.ForeignKey(ResequencingExperiment)
     mutation = models.ForeignKey(Mutation)
     percentage = models.FloatField(blank=True, null=True)
+    evidence = models.CharField(max_length=400, blank=True, null=True)
