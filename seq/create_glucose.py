aed47d199bb680013644a331bb5e30c853bd5395
Ali Ebrahim <aebrahim@ucsd.edu>
2013-03-20 13:05:51 -0700

    improve upload

--- seq/create_glucose.py
+++ seq/create_glucose.py
@@ -14,25 +14,22 @@ experiment = query_or_create(session, AleExperiment, name="glucose evolution",
 media = query_or_create(session, Media, description="Glucose M9 media", temperature=37, volume=40, stirring_speed=1123)
 freezer_box = query_or_create(session, FreezerBox, name="ale box", number=1)
 
-for i in range(11):
-    ale_id = query_or_create(session, AleId, ale_id=i, ale_experiment=experiment)
-    for j in range(3):
-        flask = query_or_create(session, Flask, ale_id=ale_id, flask_number=j + 1, media=media)
-        population_isolate = query_or_create(session, Isolate, flask=flask,
-            isolate_number=0, person="Gaby", is_population=True,
-            freezer_box=freezer_box)
-        clone = query_or_create(session, Isolate, flask=flask, isolate_number=1,
-            person="Gaby", is_population=False, freezer_box=freezer_box)
-        # each clone is made from the frozen population
-        clone.parent_isolate_id = population_isolate.id
-
-session.commit()
-
 
 # sloppy code to find sequencing experiments based off of folder names
 sequencing_path = "/home/aebrahim/sequencing/glucose_ale_reseq/"
 
-runs = [i for i in listdir(sequencing_path) if isdir(sequencing_path + i) and isfile(sequencing_path + i + "/index.html")]
+runs = [i for i in listdir(sequencing_path) if isdir(sequencing_path + i) and isfile(sequencing_path + i + "/index.html") and i.startswith("Glucose_ALE")]
+
+# add wild type
+ale_id = query_or_create(session, AleId, ale_experiment=experiment, ale_id=0)
+flask = query_or_create(session, Flask, flask_number=0,
+    ale_id=ale_id, media=media)
+isolate = query_or_create(session, Isolate, flask=flask,
+        isolate_number=0, is_population=False,
+        freezer_box=freezer_box, person="BOP27")
+session.commit()
+add_breseq_results(session, isolate.id, "BOP27", sequencing_path + "BOP27_reseq", wt=True)
+session.commit()
 
 for i in runs:
     split = i.split("_")
@@ -42,7 +39,6 @@ for i in runs:
     isolate_number = int(split[6])
     pop = split[-1] == "pop"
     print i
-    print ale_number, flask_number, isolate_number, pop
     ale_id = query_or_create(session, AleId, ale_experiment=experiment,
         ale_id=ale_number)
     flask = query_or_create(session, Flask, flask_number=flask_number,
--- seq/views.py
+++ seq/views.py
@@ -15,46 +15,66 @@ def index(request):
     experiments = ResequencingExperiment.objects.all()
     template = loader.get_template("experiment_view.html")
     context = Context({"experiments": experiments})
-    return HttpResponse(template.render(context))
+    return HttpResponse(template.render(context))er of servers in parallel or serially. These commands are grouped in tasks (regular python functions) and specified in a 'fabfile'.
+
+It is a bit like a dumbed down Capistrano, except it's in Python, dosn't expect you to be deploying Rails applications, and the 'put' command works.
+
+Unlike Capistrano, Fabric want's to stay small, light, easy to change and not bound to any specific framework.
+
+Registration Date: Fri 04 Jan 2008 07:52:24 PM UTC
+License: GNU General Public License v2 or later
+Development Status: 3 - Alpha 
 
 
 def experiment_table(request):
     experiments = ResequencingExperiment.objects.all()
+    experiment_mapping = dict((o.id, i) for i, o in enumerate(experiments))
+    # cache the urls of the experiment location
+    experiment_urls = dict((i.id, sequencing_url + i.location) for i in experiments)
     mutations = Mutation.objects.all()
     mutation_mapping = dict((mutation.id, i) for i, mutation in enumerate(mutations))
     table_header = """<tr><td>Experiment</td>"""
     for mutation in mutations:
         table_header += """<td>%d %s</td>""" % (mutation.position, mutation.sequence_change)
     table_header += "</tr>"
-    columns_base = ["""<td class="false"></td>"""] * len(mutations)
+    table_entries = [[None] * len(mutations) for i in range(len(experiments))]
+    for observed in ObservedMutation.objects.all():
+        table_entries[experiment_mapping[observed.sequencing_experiment_id]][mutation_mapping[observed.mutation_id]] = \
+            """<td class="true">%s</td>""" % observed.evidence.replace("evidence/", experiment_urls[observed.sequencing_experiment_id] + "/evidence/")
     table_body = ""
     for experiment in experiments:
-        columns = columns_base[:]  # copy
-        for observed_mutation in experiment.observedmutation_set.all():
-            mutation = observed_mutation.mutation
-            columns[mutation_mapping[mutation.id]] = """<td class="true">%s</td>""" % observed_mutation.evidence.replace("evidence/", sequencing_url + experiment.location + "/evidence/")
-            #from IPython import embed; embed()
-        table_body += """<tr><td><a href="%s">%s</a></td>%s</tr>\n""" % (sequencing_url + experiment.location, experiment.get_isolate_name(), "".join(columns))
+        table_body += """<tr><td><a href="%s">%s</a></td>""" % (sequencing_url + experiment.location, experiment.get_isolate_name())
+        for column in table_entries[experiment_mapping[experiment.id]]:
+            if column is None:
+                table_body += """<td class="false"></td>"""
+            else:
+                table_body += column
     template = loader.get_template("table_template.html")
     context = Context({"table_body": mark_safe(table_body), "title": "Experiment table", "table_header": mark_safe(table_header)})
     return HttpResponse(template.render(context))
 
 def mutation_table(request):
     experiments = ResequencingExperiment.objects.all()
-    experiment_mapping = dict((experiment, i) for i, experiment in enumerate(experiments))
+    experiment_mapping = dict((o.id, i) for i, o in enumerate(experiments))
+    # cache the urls of the experiment location
+    experiment_urls = dict((i.id, sequencing_url + i.location) for i in experiments)
     mutations = Mutation.objects.all()
+    mutation_mapping = dict((mutation.id, i) for i, mutation in enumerate(mutations))
     table_header = """<tr><td>Mutation</td><td>Gene</td><td>Protein change</td>"""
     for experiment in experiments:
         table_header += """<td>%s</td>""" % (experiment.get_isolate_name().replace("_", " "))
     table_header += "</tr>"
-    columns_base = ["""<td class="false"></td>"""] * len(experiments)
+    table_entries = [["""<td class="false"></td>"""] * len(experiments) for i in range(len(mutations))]
+    
+    observed_query = ObservedMutation.objects
+    
+    for observed in observed_query.all():
+        table_entries[mutation_mapping[observed.mutation_id]][experiment_mapping[observed.sequencing_experiment_id]] = \
+            """<td class="true">%s</td>""" % observed.evidence.replace("evidence/", experiment_urls[observed.sequencing_experiment_id] + "/evidence/")
     table_body = ""
     for mutation in mutations:
-        columns = columns_base[:]  # copy
-        for experiment in mutation.resequencingexperiment_set.all():
-            columns[experiment_mapping[experiment]] = """<td class="true">true</td>"""
         table_body += "<tr><td>%d %s</td><td>%s</td><td>%s</td>%s</tr>\n" % \
-            (mutation.position, mutation.sequence_change, mutation.gene, mutation.protein_change, "".join(columns))
+            (mutation.position, mutation.sequence_change, mutation.gene, mutation.protein_change, "".join(table_entries[mutation_mapping[mutation.id]]))
     template = loader.get_template("table_template.html")
     context = Context({"table_body": mark_safe(table_body), "title": "Mutation table", "table_header": mark_safe(table_header)})
     return HttpResponse(template.render(context))
