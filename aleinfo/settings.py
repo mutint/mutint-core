074c322047df1d1d31d17ba61cc996acf15f822e
phageghost <git@phageghost.net>
2013-05-31 14:09:15 -0700

    Changed db ENGINE to postgres and USER to ale_user

--- aleinfo/settings.py
+++ aleinfo/settings.py
@@ -15,9 +15,9 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'cellar',                      # Or path to database file if using sqlite3.
-        'USER': 'ale_server',                      # Not used with sqlite3.
+        'USER': 'ale_user',                      # Not used with sqlite3.
         'PASSWORD': 'ecoliale',                  # Not used with sqlite3.
         'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
