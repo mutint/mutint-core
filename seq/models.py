974b73602bcf484fa14acae824df7a86b8a21f75
phageghost <git@phageghost.net>
2013-06-07 14:34:49 -0700

    Changed name of statistic field

--- seq/models.py
+++ seq/models.py
@@ -47,7 +47,7 @@ class ObservedMutation(models.Model):
     wt_reads = models.IntegerField(null=True)
     mutated_reads = models.IntegerField(null=True)
     other_reads = models.IntegerField(null=True)
-    genome_likelihood_mutant = models.FloatField(null=True)
+    mutant_genome_likelihood = models.FloatField(null=True)
     sequencing_experiment = models.ForeignKey(ResequencingExperiment)
     mutation = models.ForeignKey(Mutation)
     evidence = models.CharField(max_length=400, blank=True, null=True)
