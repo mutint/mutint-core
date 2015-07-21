c16833098c47948937d84fb27a81943a1b775245
Patrick Phaneuf <pphaneuf@eng.ucsd.edu>
2015-07-21 09:39:13 -0700

    Adding unit tests that had forgotten to add previously.

---
+++ upload/tests/test_upload.py
@@ -0,0 +1,24 @@
+__author__ = 'pphaneuf'
+
+import unittest
+
+from upload.upload import is_sample_clonal_or_popuation
+from upload.upload import Breseq_sample_type
+
+class TestUpload(unittest.TestCase):
+
+    def test_is_sample_clonal_or_popuation_clonal(self):
+
+        log_file_path = "log_sample_is_clonal.txt"
+
+        sample_type = is_sample_clonal_or_popuation(log_file_path)
+
+        self.assertEquals(Breseq_sample_type.clonal, sample_type)
+
+    def test_is_sample_clonal_or_popuation_population(self):
+
+        log_file_path = "log_sample_is_population.txt"
+
+        sample_type = is_sample_clonal_or_popuation(log_file_path)
+
+        self.assertEquals(Breseq_sample_type.population, sample_type)
