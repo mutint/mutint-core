f815ede40a93f5d9eb26cfdb03784a403e6d9942
Patrick Phaneuf <pphaneuf@eng.ucsd.edu>
2015-08-14 12:00:11 -0700

    Now outputting all mutations to a CSV file for download.

--- mutation_db_browser/views.py
+++ mutation_db_browser/views.py
@@ -1,13 +1,24 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
-from ale.models import *
 from seq.models import *
+
+from mutation_db_browser.CsvUnicodeWriter import CsvUnicodeWriter
 
 
 @login_required
 def mutations_db_browser(request):
+
+    content_disposition_type_str = 'attachment'
+    content_disposition_separator = ";"
+    content_disposition_file_name_attribute = 'filename="ale_mutations_db.csv"'
+    content_disposition_str = content_disposition_type_str\
+                              + content_disposition_separator\
+                              + content_disposition_file_name_attribute
+
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = content_disposition_str
+    writer = CsvUnicodeWriter(response)
 
     reseq_experiments = ResequencingExperiment.objects.all()
 
@@ -15,26 +26,22 @@ def mutations_db_browser(request):
 
     observed_mutations = ObservedMutation.objects.filter(sequencing_experiment_id__in=experiment_mapping.keys())
 
-    output_str = ""
-
     for observed_mutation in observed_mutations:
 
-        output_str += _build_mutation_output_str(observed_mutation) + "<br>"
+        writer.writerow(_build_mutation_output_str(observed_mutation))
 
-    return HttpResponse(output_str)
+    return response
 
 
 def _build_mutation_output_str(observed_mutation):
 
     ale_experiment_str = observed_mutation.sequencing_experiment.isolate.flask.ale_id.ale_experiment.name
+
     seq_experiment_str = observed_mutation.sequencing_experiment.get_isolate_name()
-    observed_mutation_str = observed_mutation.mutation.mutation_type\
-                             + " " + str(observed_mutation.mutation.position)\
-                             + " " + observed_mutation.mutation.sequence_change\
-                             + " " + observed_mutation.mutation.gene
 
-    output_str = ale_experiment_str\
-                 + " " + seq_experiment_str\
-                 + " " + observed_mutation_str
+    observed_mutation_str_list = [observed_mutation.mutation.mutation_type,
+                                  str(observed_mutation.mutation.position),
+                                  observed_mutation.mutation.sequence_change,
+                                  observed_mutation.mutation.gene]
 
-    return output_str
+    return [ale_experiment_str, seq_experiment_str] + observed_mutation_str_list
