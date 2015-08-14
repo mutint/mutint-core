590c1e2e0a7f1bb855e5a0a07d2681ec026dc94b
Patrick Phaneuf <pphaneuf@eng.ucsd.edu>
2015-08-14 11:20:49 -0700

    Printing out all of the mutations within the DB.

--- mutation_db_browser/views.py
+++ mutation_db_browser/views.py
@@ -2,26 +2,39 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
+from ale.models import *
 from seq.models import *
 
 
 @login_required
 def mutations_db_browser(request):
 
-    '''
-    list_of_experiments = ResequencingExperiment.objects.all()
+    reseq_experiments = ResequencingExperiment.objects.all()
 
-    for experiment in experiments:
-        print(experiment.mutations)
-    '''
+    experiment_mapping = dict((o.id, o) for o in reseq_experiments)
 
-    mutations = Mutation.objects.all()
+    observed_mutations = ObservedMutation.objects.filter(sequencing_experiment_id__in=experiment_mapping.keys())
 
-    for mutation in mutations:
-        print(mutation.mutation_type)
+    output_str = ""
 
-    return HttpResponse("mutation_db_browser")
+    for observed_mutation in observed_mutations:
 
-    
+        output_str += _build_mutation_output_str(observed_mutation) + "<br>"
 
-    
+    return HttpResponse(output_str)
+
+
+def _build_mutation_output_str(observed_mutation):
+
+    ale_experiment_str = observed_mutation.sequencing_experiment.isolate.flask.ale_id.ale_experiment.name
+    seq_experiment_str = observed_mutation.sequencing_experiment.get_isolate_name()
+    observed_mutation_str = observed_mutation.mutation.mutation_type\
+                             + " " + str(observed_mutation.mutation.position)\
+                             + " " + observed_mutation.mutation.sequence_change\
+                             + " " + observed_mutation.mutation.gene
+
+    output_str = ale_experiment_str\
+                 + " " + seq_experiment_str\
+                 + " " + observed_mutation_str
+
+    return output_str
