9e903cd347f39eb9d9ad9832dc8a58dc8cb19fc3
Patrick Phaneuf <pphaneuf@eng.ucsd.edu>
2015-08-21 09:08:45 -0700

    Refactored

--- seq/views.py
+++ seq/views.py
@@ -15,6 +15,13 @@ REQUEST_ALE_EXPERIMENT_ID = "ale_experiment_id"
 REQUEST_ALE_NUMBER = "ale_no"
 REQUEST_ALL = "all"
 
+HTML_MUTATION_TABLE_HEADER = """<tr><td>Mutation</td><td>Gene</td><td>Protein change</td>"""
+HTML_MUTATION_TABLE_ROW = """<td>%d %s<a href="javascript:void(0)" class="shut" style="float:right;display:none;" onclick="deleteRow.call(this)"><img src="/static/DataTables/media/images/close-icon.gif" width="12" height="11"></a></td>"""
+
+EXPERIMENT_MAPPING_FILTERING_SHOW_FLAG = "show"
+EXPERIMENT_MAPPING_FILTERING_REMOVE_FLAG = "remove"
+
+
 if hasattr(settings, "sequencing_url"):
     reseqencing_report_url = settings.sequencing_url
 else:
@@ -92,6 +99,51 @@ def lists(request):
     return HttpResponse(template.render(context))
 
 
+@login_required
+def mutation_table(request):
+
+    experiment_ids = _get_experiment_id(request)
+
+    ale_number = _get_ale_number(request)
+
+    experiment_list = _get_experiment_list(experiment_ids)
+
+    experiment_mapping = _get_experiment_mapping(request)
+
+    experiment_mapping = _filter_checked_flasks(request, experiment_mapping)
+
+    table_header = _get_table_header(experiment_mapping)
+
+    table_body = _get_table_body(experiment_mapping, request)
+
+    template = loader.get_template("table_template.html")
+
+    context = Context({"experiments": experiment_list,
+                       "ale_no": ale_number,
+                       "experiment_id": experiment_ids,
+                       "table_body": mark_safe(table_body),
+                       "title": "Mutation table",
+                       "table_header": mark_safe(table_header)})
+
+    return HttpResponse(template.render(context))
+
+
+@login_required
+def isolate_list(request):
+    ale_experiment_id = request.GET.get(REQUEST_ALE_EXPERIMENT_ID)
+    if ale_experiment_id is None:
+        isolates = Isolate.objects.all()
+    else:
+        ale_experiment_id = int(ale_experiment_id)
+        isolates = Isolate.objects.raw(
+            """SELECT isolate_id AS id FROM id_mapping WHERE
+            experiment_id=%d;""" % ale_experiment_id)
+        """return a list of resequencing experiments"""
+    template = loader.get_template("isolate_view.html")
+    context = Context({"isolates": isolates, "seq_url": reseqencing_report_url})
+    return HttpResponse(template.render(context))
+
+
 def _get_experiment_info_list(experiments):
     experiments_info_list = []
 
@@ -161,56 +213,23 @@ def experiment_table(request):
     return HttpResponse(template.render(context))
 
 
-@login_required
-def mutation_table(request):
+def _get_experiment_urls(experiment_mapping):
 
-    # Get the full list of ale experiments for the ale number of interest
-    experiment_id = request.GET.get(REQUEST_ALE_EXPERIMENT_ID)
-    experiment_id = None if experiment_id is None or experiment_id == "all" else int(experiment_id)
-
-    ale_no = request.GET.get(REQUEST_ALE_NUMBER)
-    ale_no = None if ale_no is None or ale_no == "all" else int(ale_no)
-
-    if experiment_id is not None:
-        experiment = AleExperiment.objects.get(ale_id=experiment_id)
-        list_of_experiments = experiment.aleid_set.only("ale_id")
-    else:
-        list_of_experiments = ResequencingExperiment.objects.all()
-
-    extra_validation = False if request.GET.get("novalid") else True
-
-    experiments = _get_seq_experiments(request)
-
-    # experiment_mapping = dict((o.id, o) for i, o in enumerate(experiments) if o.isolate.__unicode__().find("POP")==-1)
-    experiment_mapping = dict((o.id, o) for o in experiments)
-
-    # Show checked flasks only
-    query_string = request.GET.get("show")
-    if query_string is not None:
-        checked_experiment_ids = query_string.encode('latin_1').replace("{", "").replace("}", "")
-        if checked_experiment_ids != "":
-            checked_experiment_id_list = [int(i) for i in checked_experiment_ids.split(",") if i != ""]
-            checked_experiment_ids = experiment_mapping.keys()
-            for checked_experiment_id in checked_experiment_ids:
-                if checked_experiment_id not in checked_experiment_id_list:
-                    del experiment_mapping[checked_experiment_id]
-
-    # Remove checked flasks
-    query_string = request.GET.get("remove")
-    if query_string is not None:
-        checked_experiment_ids = query_string.encode('latin_1').replace("{", "").replace("}", "")
-        if checked_experiment_ids != "":
-            for checked_experiment_id in checked_experiment_ids.split(","):
-                if checked_experiment_id != "":
-                    del experiment_mapping[int(checked_experiment_id)]
-
-    # cache the urls of the experiment location
     experiment_urls = dict((i.id, reseqencing_report_url + i.location) for i in experiment_mapping.values())
-    observed_mutations = ObservedMutation.objects.filter(sequencing_experiment_id__in=experiment_mapping.keys())
-    mutations = Mutation.objects.filter(pk__in=observed_mutations.values_list("mutation", flat=True))
-    mutation_mapping = dict((id, i) for i, id in enumerate(mutations.values_list("id", flat=True)))
 
-    table_header = """<tr><td>Mutation</td><td>Gene</td><td>Protein change</td>"""
+    return experiment_urls
+
+
+def _get_observed_mutations(experiment_mapping):
+
+    observed_mutations = ObservedMutation.objects.filter(sequencing_experiment_id__in=experiment_mapping.keys())
+
+    return observed_mutations
+
+
+def _get_table_header(experiment_mapping):
+
+    table_header = HTML_MUTATION_TABLE_HEADER
 
     for checked_experiment_id in sorted(experiment_mapping):
 
@@ -220,16 +239,29 @@ def mutation_table(request):
                               + " "\
                               + experiment.get_isolate_name().replace("_", " ")
 
-        # Add checkbox to each column
         table_header += """<td><input type="checkbox" class="cb" name=%s /><br>%s</td>""" % (
             experiment.id,
             mutation_identifier)
 
     table_header += "</tr>"
 
-    table_entries = [["""<td class="false"></td>"""] * len(experiment_mapping) for i in range(len(mutations))]
+    return table_header
+
+
+def _get_table_body(experiment_mapping, request):
+
+    observed_mutations = _get_observed_mutations(experiment_mapping)
+
+    mutations = Mutation.objects.filter(pk__in=observed_mutations.values_list("mutation", flat=True))
+    mutation_mapping = dict((id, i) for i, id in enumerate(mutations.values_list("id", flat=True)))
+
+    experiment_urls = _get_experiment_urls(experiment_mapping)
 
     experiment_mapping = dict((o, i) for i, o in enumerate(sorted(experiment_mapping.keys())))
+
+    extra_validation = False if request.GET.get("novalid") else True
+
+    table_entries = [["""<td class="false"></td>"""] * len(experiment_mapping) for i in range(len(mutations))]
 
     for observed_mutation in observed_mutations:
 
@@ -254,8 +286,9 @@ def mutation_table(request):
             continue
 
         else:
-            table_row += """<td>%d %s<a href="javascript:void(0)" class="shut" style="float:right;display:none;" onclick="deleteRow.call(this)"><img src="/static/DataTables/media/images/close-icon.gif" width="12" height="11"></a></td>""" % (
-                mutation.position, mutation.sequence_change)
+            table_row += HTML_MUTATION_TABLE_ROW % (
+                mutation.position,
+                mutation.sequence_change)
 
         table_row += "<td>%s</td>" % mutation.gene
         table_row += "<td>%s</td>" % mutation.protein_change
@@ -263,31 +296,80 @@ def mutation_table(request):
         table_row += "</tr>"
         table_body += table_row + "\n"
 
-    template = loader.get_template("table_template.html")
-    context = Context({"experiments": list_of_experiments,
-                       "ale_no": ale_no,
-                       "experiment_id": experiment_id,
-                       "table_body": mark_safe(table_body),
-                       "title": "Mutation table",
-                       "table_header": mark_safe(table_header)})
-
-    return HttpResponse(template.render(context))
+    return table_body
 
 
-@login_required
-def isolate_list(request):
-    ale_experiment_id = request.GET.get(REQUEST_ALE_EXPERIMENT_ID)
-    if ale_experiment_id is None:
-        isolates = Isolate.objects.all()
+def _get_experiment_id(request):
+
+    # Get the full list of ale experiments for the ale number of interest
+    experiment_ids = request.GET.get(REQUEST_ALE_EXPERIMENT_ID)
+    experiment_ids = None if experiment_ids is None or experiment_ids == "all" else int(experiment_ids)
+
+    return experiment_ids
+
+
+def _get_ale_number(request):
+
+    ale_number = request.GET.get(REQUEST_ALE_NUMBER)
+    ale_number = None if ale_number is None or ale_number == "all" else int(ale_number)
+
+    return ale_number
+
+
+def _get_experiment_list(experiment_ids):
+
+    if experiment_ids is not None:
+        experiment = AleExperiment.objects.get(ale_id=experiment_ids)
+        experiment_list = experiment.aleid_set.only("ale_id")
     else:
-        ale_experiment_id = int(ale_experiment_id)
-        isolates = Isolate.objects.raw(
-            """SELECT isolate_id AS id FROM id_mapping WHERE
-            experiment_id=%d;""" % ale_experiment_id)
-        """return a list of resequencing experiments"""
-    template = loader.get_template("isolate_view.html")
-    context = Context({"isolates": isolates, "seq_url": reseqencing_report_url})
-    return HttpResponse(template.render(context))
+        experiment_list = ResequencingExperiment.objects.all()
+
+    return experiment_list
+
+
+def _get_experiment_mapping(request):
+
+    experiments = _get_seq_experiments(request)
+
+    experiment_mapping = dict((o.id, o) for o in experiments)
+
+    return experiment_mapping
+
+
+def _show_checked_flasks(request, experiment_mapping):
+
+    query_string = request.GET.get(EXPERIMENT_MAPPING_FILTERING_SHOW_FLAG)
+    if query_string is not None:
+        checked_experiment_ids = query_string.encode('latin_1').replace("{", "").replace("}", "")
+        if checked_experiment_ids != "":
+            checked_experiment_id_list = [int(i) for i in checked_experiment_ids.split(",") if i != ""]
+            checked_experiment_ids = experiment_mapping.keys()
+            for checked_experiment_id in checked_experiment_ids:
+                if checked_experiment_id not in checked_experiment_id_list:
+                    del experiment_mapping[checked_experiment_id]
+
+    return experiment_mapping
+
+def _remove_checked_flasks(request, experiment_mapping):
+
+    query_string = request.GET.get(EXPERIMENT_MAPPING_FILTERING_REMOVE_FLAG)
+    if query_string is not None:
+        checked_experiment_ids = query_string.encode('latin_1').replace("{", "").replace("}", "")
+        if checked_experiment_ids != "":
+            for checked_experiment_id in checked_experiment_ids.split(","):
+                if checked_experiment_id != "":
+                    del experiment_mapping[int(checked_experiment_id)]
+
+    return experiment_mapping
+
+
+def _filter_checked_flasks(request, experiment_mapping):
+
+    experiment_mapping = _show_checked_flasks(request, experiment_mapping)
+
+    experiment_mapping = _remove_checked_flasks(request, experiment_mapping)
+
+    return experiment_mapping
 
 
 # Provide a table showing frequencies of each observed mutation in the lineage
