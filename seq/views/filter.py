30f77ceade43f7f9c98b26a06e81573dac226399
dgosting <dgosting@ucsd.edu>
2016-03-03 17:43:57 -0800

    filters out genes specified in ignore_gene_list

--- seq/views/filter.py
+++ seq/views/filter.py
@@ -16,15 +16,23 @@ from bs4 import BeautifulSoup as soup
 
 INDEX_TEMPLATE = "filter.html"
 
-
 if hasattr(settings, common.SETTINGS_SEQUENCING_URL):
     reseqencing_report_url = settings.sequencing_url
 else:
     reseqencing_report_url = common.DEFAULT_RESEQ_REPORT_URL
 
 
-class filterForm(forms.Form):
-    filter_field = forms.IntegerField(max_value=100, label="Minimum % Cutoff ", min_value=0, initial=5)
+class minForm(forms.Form):
+    filter_field = forms.IntegerField(max_value=100, label="Minimum % Cutoff ", min_value=0, initial=20)
+
+
+class maxForm(forms.Form):
+    filter_field = forms.IntegerField(max_value=100, label="Maximum % Cutoff ", min_value=0, initial=100)
+
+class ignoreGenesForm(forms.Form):
+    filter_field = forms.CharField()
+
+
 
 @login_required
 def filter(request):
@@ -32,36 +40,45 @@ def filter(request):
 
     template = loader.get_template(INDEX_TEMPLATE)
 
+    ignored_genes = ["test", "other", 'first']
+
     context = Context({
-        "form": filterForm(),
+        "minform": minForm(),
+        "maxform": maxForm(),
+        "gene_form": ignoreGenesForm(),
+        "ignored_genes": ignored_genes,
         "ale_experiment_name": ale_experiment_name
     })
 
     return HttpResponse(template.render(context))
 
 
-def filter_table(table, cutoff=0.05):
-
+def filter_table(table, min_cutoff=0.20, max_cutoff=1.00, ignore_gene_list = []):
+    ignore_gene_list = ["icd"]
     soup_table = soup(table, from_encoding='utf-8')
     filtered_table = ""
     rows = soup.find_all(soup_table, 'tr')
     for row in rows:
         table_row = "<tr>"
         cell = row.find_all('td')
-        matches = 0
+        skip_row = False
         for entry in cell:
             entry.find_all('class')
             text = entry.get_text()
+            for gene in ignore_gene_list:
+                if gene == text:
+                    skip_row = True
             try:
                 element = float(text)
-                if element > cutoff:
-                    matches += 1
+                if min_cutoff <= element <= max_cutoff:
                     table_row += entry.encode('utf-8')
+                else:
+                    skip_row = True
             except ValueError:
                 table_row += entry.encode('utf-8')
-        table_row += "</tr>"
-        if matches <= 1:
+        if skip_row is True:
             continue
+        table_row += "</tr>"
         filtered_table += table_row + "\n"
 
     return filtered_table
