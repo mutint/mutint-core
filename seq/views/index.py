e2dfe81f9415f92c1f9e90014d7a2500a0aa67ae
Patrick Phaneuf <pphaneuf@eng.ucsd.edu>
2016-04-10 17:36:19 -0700

    now showing stats for ALE unique mutations on main page

--- seq/views/index.py
+++ seq/views/index.py
@@ -4,6 +4,8 @@ from django.template import Context, loader
 
 from ale.models import *    # TODO: only import necessary models.
 
+import seq.models
+
 from django.contrib.auth.decorators import login_required
 
 import aleinfo.settings as settings
@@ -15,6 +17,8 @@ __author__ = 'pphaneuf'
 
 INDEX_TEMPLATE = "index.html"
 
+MUTATION_TYPE_LIST = ['SNP', 'SUB', 'DEL', 'INS', 'MOB', 'AMP', 'CON', 'INV']
+
 
 if hasattr(settings, common.SETTINGS_SEQUENCING_URL):
     reseqencing_report_url = settings.sequencing_url
@@ -24,13 +28,18 @@ else:
 
 @login_required
 def index(request):
-    """display a list of ales with links to the resequencing"""
+
+    mutation_type_count_dict = {}
+    for mutation_type in MUTATION_TYPE_LIST:
+        mutation_type_count = seq.models.Mutation.objects.filter(mutation_type=mutation_type).count()
+        mutation_type_count_dict[mutation_type] = mutation_type_count
 
     ale_experiments = AleExperiment.objects.all()
 
     template = loader.get_template(INDEX_TEMPLATE)
 
-    context = Context({"experiments": ale_experiments,
+    context = Context({"mutation_type_count_dict": mutation_type_count_dict,
+                       "experiments": ale_experiments,
                        "seq_url": reseqencing_report_url})
 
     return HttpResponse(template.render(context))
