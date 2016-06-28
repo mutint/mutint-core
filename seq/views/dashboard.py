3c120b68febac6826ef4ff3e81ef538932711394
denn <dgosting@gmail.com>
2016-06-27 18:20:04 -0700

    added bar graph of all Observed Mutaitons

--- seq/views/dashboard.py
+++ seq/views/dashboard.py
@@ -11,6 +11,10 @@ from seq.views import common
 
 import seq.models
 
+from django.db.models import Count
+
+from django.utils.safestring import mark_safe
+
 
 DASHBOARD_TEMPLATE = "dashboard.html"
 
@@ -27,8 +31,45 @@ def dashboard(request):
         protein_change_count = seq.models.Mutation.objects.filter(protein_change__contains=protein_change_type).count()
         protein_change_type_count_dict[protein_change_type] = protein_change_count
 
-    context = Context({"protein_change_type_count_dict": protein_change_type_count_dict,
-                       "mutation_type_count_dict": mutation_type_count_dict})
+    genes = seq.models.ObservedMutation.objects.values('mutation__gene').annotate(the_count=Count('mutation__gene')).order_by('-the_count')
+    sequence_changes = seq.models.ObservedMutation.objects.values('mutation__sequence_change', 'mutation__gene').annotate(the_count=Count('mutation__gene')).order_by('-the_count')
+
+    if 'number_of_top_genes' in request.GET:
+
+        gene_query = request.GET['number_of_top_genes']
+
+        if _is_query_empty(gene_query):
+            genes_to_show = list(genes[:20])
+            sequence_changes_to_show = list(sequence_changes[:20])
+
+        else:
+            genes_to_show = list(genes[:int(request.GET['number_of_top_genes'])])
+            sequence_changes_to_show = list(sequence_changes[:int(request.GET['number_of_top_genes'])])
+
+        context = Context({"protein_change_type_count_dict": protein_change_type_count_dict,
+                           "mutation_type_count_dict": mutation_type_count_dict,
+                           "genes": mark_safe(genes_to_show),
+                           "sequence_changes": mark_safe(sequence_changes_to_show)})
+
+    else:
+        genes_to_show = list(genes[:20])
+        sequence_changes_to_show = list(sequence_changes[:20])
+        context = Context({"protein_change_type_count_dict": protein_change_type_count_dict,
+                           "mutation_type_count_dict": mutation_type_count_dict,
+                           "genes": mark_safe(genes_to_show),
+                           "sequence_changes": mark_safe(sequence_changes_to_show)})
 
     template = loader.get_template(DASHBOARD_TEMPLATE)
     return HttpResponse(template.render(context))
+
+
+def _is_query_empty(query):
+
+    is_query_empty = False
+
+    if not query:
+
+        is_query_empty = True
+
+    return is_query_empty
+
