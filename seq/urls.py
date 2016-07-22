96524c1cd8b27aeb2a2269cfc1da3f9cbc6eb13a
Patrick Phaneuf <pphaneuf@eng.ucsd.edu>
2016-07-22 10:13:21 -0700

    removed lineage page

--- seq/urls.py
+++ seq/urls.py
@@ -4,11 +4,8 @@ import seq.views.index
 import seq.views.mutations
 import seq.views.key_mutations
 import seq.views.common_mutations
-import seq.views.select_lineage
-import seq.views.lineage
 import seq.views.meta_data
 import seq.views.search
-# import seq.views.filter
 import seq.views.gene
 import seq.views.duplication
 import seq.views.dashboard
@@ -20,9 +17,7 @@ urlpatterns = patterns('',
                        url('^mutations$', seq.views.mutations.mutation_table, name="mutation_table"),
                        url('^key_mutations', seq.views.key_mutations.key_mutations, name="key_mutations"),
                        url('^common_mutations', seq.views.common_mutations.common_mutations, name="common_mutations"),
-                       url('^select_lineage', seq.views.select_lineage.select_lineage, name="select_lineage"),
-                       url('^lineage', seq.views.lineage.lineage, name="lineage"),
-                       url('^meta_data', seq.views.meta_data.meta_data, name="lineage"),
+                       url('^meta_data', seq.views.meta_data.meta_data, name="meta_data"),
                        url('^search', seq.views.search.search, name="search"),
                        url('^gene', seq.views.gene.gene, name="gene"),
                        url('^duplication', seq.views.duplication.duplication, name="duplication"),
