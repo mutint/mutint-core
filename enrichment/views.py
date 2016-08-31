38d5fb41aad8a4d5dacb56626355ab4b18a7cbb2
pvphaneuf <pphaneuf@eng.ucsd.edu>
2016-08-31 12:13:56 -0700

    now building ordered table of enrichment mutations in shared enrichment genes

--- enrichment/views.py
+++ enrichment/views.py
@@ -5,13 +5,15 @@ from django.utils.safestring import mark_safe
 import seq.models
 import seq.views.common
 from common.db_util import get_ordered_reseq_dict, get_all_ale_experiments, get_recent_experiments
-# TODO: The mutation table build should use the factory pattern.
-from seq.views import mutation_table_builder
+from seq.views import mutation_table_builder  # TODO: The mutation table build should use the factory pattern.
 import metadata.views
 from enrichment.models import EnrichmentMutation
+from seq.models import ObservedMutation
+from seq.models import ResequencingExperiment
 from common.constants import REQUEST_MUTATION_ID, REQUEST_ALE_EXPERIMENT_ID
 from filter import mutation_filter
 from common.util import check_hidden_columns_and_filters
+from collections import OrderedDict
 
 HTML_MUTATION_TABLE_HEADER = """<tr><td></td><td>Position</td><td>Mutation Type</td><td>Sequence Change</td><td>Gene</td><td>Function</td><td>Product</td><td>GO Process</td><td>GO Component</td><td>Protein change</td>"""
 
@@ -59,53 +61,77 @@ def enrichment_mutations(request):
 
 
 # TODO: needs to be refactored. Currently has poor performance. Should use queryset to filter instead of for loops.
+# TODO: ensure that the local and global filters are used to remove unwanted mutations
+# TODO: ensure that the table functionality with checkboxes work.
+# TODO: filter out starting-strain mutations
 @login_required
 def shared_enrichment_mutations(request):
 
     mutation_id = request.GET.get(REQUEST_MUTATION_ID)
 
     enrichment_mutation_queryset = EnrichmentMutation.objects.filter(mutation_id=mutation_id)
-    # mutation_id_list = [enrichment_mutation.mutation_id for enrichment_mutation in enrichment_mutation_queryset]
-    # mutation_queryset = seq.models.Mutation.objects.filter(id__in=mutation_id_list)
-    enrichment_mutation_list = [enrichment_mutation.mutation for enrichment_mutation in enrichment_mutation_queryset]
+    enrichment_mutation = enrichment_mutation_queryset[0]  # There should only be one enrichment mutation per mutation_id
+    enriched_gene = enrichment_mutation.mutation.gene
 
-    table_header = HTML_MUTATION_TABLE_HEADER
-    enrichment_mutation = enrichment_mutation_list[0]  # Should only be 1 enrichment mutation
-    table_body = "<tr>"
-    table_body += mutation_table_builder.HTML_MUTATION_TABLE_ROW
-    table_body += "<td>%s</td>" % enrichment_mutation.position
-    table_body += "<td>%s</td>" % enrichment_mutation.mutation_type
-    table_body += "<td>%s</td>" % enrichment_mutation.sequence_change
-    table_body += "<td><a href=/gene?g=%s>%s</a></td>" % (enrichment_mutation.gene, enrichment_mutation.gene)
-    table_body += "<td>%s</td>" % ("" if enrichment_mutation.function is None else enrichment_mutation.function)
-    table_body += "<td>%s</td>" % ("" if enrichment_mutation.product is None else enrichment_mutation.product)
-    table_body += "<td>%s</td>" % ("" if enrichment_mutation.go_process is None else enrichment_mutation.go_process)
-    table_body += "<td>%s</td>" % ("" if enrichment_mutation.go_component is None else enrichment_mutation.go_component)
-    table_body += "<td>%s</td>" % enrichment_mutation.protein_change
-    table_body += "</tr>"
+    enriched_mutation_queryset = EnrichmentMutation.objects.filter(mutation__gene=enriched_gene)
+    observed_mutation_queryset = ObservedMutation.objects.filter(mutation__in=enriched_mutation_queryset.values('mutation'))
 
+    ordered_reseq_queryset = ResequencingExperiment.objects.all().order_by('tech_rep__isolate__flask__ale_id__ale_experiment__name',
+                                                                           'tech_rep__isolate__flask__ale_id__ale_id',
+                                                                           'tech_rep__isolate__flask__flask_number',
+                                                                           'tech_rep__isolate__isolate_number')
+    ordered_reseq_queryset = ordered_reseq_queryset.filter(
+        id__in=observed_mutation_queryset.values('sequencing_experiment'))
+
+    # TODO: Have to order the reseqs somehow.
+    ordered_reseq_dict = OrderedDict((reseq.id, reseq) for reseq in ordered_reseq_queryset)
+    table_header = mutation_table_builder.get_table_header(ordered_reseq_dict)
+
+    table_body = mutation_table_builder.get_table_body(ordered_reseq_dict,
+                                                       observed_mutation_queryset,
+                                                       table_type=mutation_table_builder.TableType.SHARED)
+
+    # # mutation_id_list = [enrichment_mutation.mutation_id for enrichment_mutation in enrichment_mutation_queryset]
+    # # mutation_queryset = seq.models.Mutation.objects.filter(id__in=mutation_id_list)
+    # enrichment_mutation_list = [enrichment_mutation.mutation for enrichment_mutation in enrichment_mutation_queryset]
+    #
+    # table_header = HTML_MUTATION_TABLE_HEADER
+    # enrichment_mutation = enrichment_mutation_list[0]  # Should only be 1 enrichment mutation
+    # table_body = "<tr>"
+    # table_body += mutation_table_builder.HTML_MUTATION_TABLE_ROW
+    # table_body += "<td>%s</td>" % enrichment_mutation.position
+    # table_body += "<td>%s</td>" % enrichment_mutation.mutation_type
+    # table_body += "<td>%s</td>" % enrichment_mutation.sequence_change
+    # table_body += "<td><a href=/gene?g=%s>%s</a></td>" % (enrichment_mutation.gene, enrichment_mutation.gene)
+    # table_body += "<td>%s</td>" % ("" if enrichment_mutation.function is None else enrichment_mutation.function)
+    # table_body += "<td>%s</td>" % ("" if enrichment_mutation.product is None else enrichment_mutation.product)
+    # table_body += "<td>%s</td>" % ("" if enrichment_mutation.go_process is None else enrichment_mutation.go_process)
+    # table_body += "<td>%s</td>" % ("" if enrichment_mutation.go_component is None else enrichment_mutation.go_component)
+    # table_body += "<td>%s</td>" % enrichment_mutation.protein_change
+    # table_body += "</tr>"
+    #
     # Get the reseq's that are part of the ALE experiments from the enrichment_mutation_queryset
-    enrichment_mutation_ale_exp_list = [enrichment_mutation.ale_experiment for enrichment_mutation in enrichment_mutation_queryset]
-    all_reseq_queryset = seq.models.ResequencingExperiment.objects.all()
-    ale_experiment_reseq_list = []
-    for reseq in all_reseq_queryset:
-        if reseq.ale_experiment in enrichment_mutation_ale_exp_list:
-            ale_experiment_reseq_list.append(reseq)
-
-    # filter reseq for only those that contain key mutation
-    observed_mutation_queryset = seq.models.ObservedMutation.objects.filter(sequencing_experiment__in=ale_experiment_reseq_list)
-    enrichment_mutation_reseq_list = []
-    for observed_mutation in observed_mutation_queryset:
-        if observed_mutation.mutation in enrichment_mutation_list:
-            enrichment_mutation_reseq_list.append(observed_mutation.sequencing_experiment)
-
-    reseq_info_list = metadata.views.get_reseq_info_list(enrichment_mutation_reseq_list)
+    # enrichment_mutation_ale_exp_list = [enrichment_mutation.ale_experiment for enrichment_mutation in enrichment_mutation_queryset]
+    # all_reseq_queryset = seq.models.ResequencingExperiment.objects.all()
+    # ale_experiment_reseq_list = []
+    # for reseq in all_reseq_queryset:
+    #     if reseq.ale_experiment in enrichment_mutation_ale_exp_list:
+    #         ale_experiment_reseq_list.append(reseq)
+    #
+    # # filter reseq for only those that contain enriched mutation
+    # observed_mutation_queryset = seq.models.ObservedMutation.objects.filter(sequencing_experiment__in=ale_experiment_reseq_list)
+    # enrichment_mutation_reseq_list = []
+    # for observed_mutation in observed_mutation_queryset:
+    #     if observed_mutation.mutation in enrichment_mutation_list:
+    #         enrichment_mutation_reseq_list.append(observed_mutation.sequencing_experiment)
+    #
+    # reseq_info_list = metadata.views.get_reseq_info_list(enrichment_mutation_reseq_list)
 
     template = loader.get_template("enrichment/shared_enrichment_mutations.html")
-    context = {"title": "Shared Enrichment Mutations",
+    context = {"title": "Shared Enriched Genes",
                "table_header": mark_safe(table_header),
                "table_body": mark_safe(table_body),
-               "reseq_info_list": reseq_info_list,
+               # "reseq_info_list": reseq_info_list,
                "experiments": get_all_ale_experiments(),
                "recent_experiments": get_recent_experiments()}
 
