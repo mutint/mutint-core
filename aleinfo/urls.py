3a910fabe34e2f6c4a7dea07dbf029d944b8642f
dgosting <dgosting@gmail.com>
2016-09-01 11:01:49 -0700

    added custom login page

--- aleinfo/urls.py
+++ aleinfo/urls.py
@@ -2,16 +2,14 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from django.conf.urls import include, url
 
-import django.contrib.auth.views
-
 urlpatterns = [
-    url(r'^accounts/login/$', django.contrib.auth.views.login),
+    url(r'^accounts/login/$', include('login.urls')),
     url(r'^', include('seq.urls')),
     url(r'^filter/', include('filter.urls')),
     url(r'^fixation/', include('fixation.urls')),
     url(r'^stats', include('stats.urls')),
     url(r'^metadata', include('metadata.urls')),
-    url(r'^enrichment/', include('enrichment.urls'))
+    url(r'^enrichment/', include('enrichment.urls')),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
