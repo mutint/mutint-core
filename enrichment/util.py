5747c03d26c0fc317db45e3b69afbc0e29452c14
pvphaneuf <pphaneuf@eng.ucsd.edu>
2016-09-03 10:22:55 -0700

    update on handling intergenic genes in enrichment analysis

--- enrichment/util.py
+++ enrichment/util.py
@@ -13,8 +13,10 @@ def get_enrichment_mutation_list(ale_experiment_id):
     ale_exp_reseq_mutation_lists = get_ale_exp_reseq_mutation_lists(reseq_dict)  # Will remove starting strain mutations.
     filter_settings = get_filter_settings(ale_experiment_id)  #TODO: may not matter since the experiment isn't yet established on the website.
     ignored_mutation_id_list = _get_ignored_mutation_id_list(filter_settings)
+
     mutation_gene_count_dict = _get_mutation_gene_count_dict(ale_exp_reseq_mutation_lists, ignored_mutation_id_list)
     enrichment_mutation_list = _get_enrichment_mutation_list(ale_exp_reseq_mutation_lists, ignored_mutation_id_list, mutation_gene_count_dict)
+
     return enrichment_mutation_list
 
 
@@ -30,18 +32,14 @@ def _get_mutation_gene_count_dict(ale_experiment_mutation_list, ignored_mutation
             if mutation.id not in ignored_mutation_id_list:
 
                 if INTERGENIC_SPLIT_CHAR in mutation.gene:
-                    _process_intergenic_mutation_gene_count(mutation.gene, mutation_gene_count_dict)
+                    gene_list = mutation.gene.split(INTERGENIC_SPLIT_CHAR)
                 else:
-                    mutation_gene_count_dict[mutation.gene] += 1
+                    gene_list = [mutation.gene]
+
+                for gene in gene_list:
+                    mutation_gene_count_dict[gene] += 1
 
     return mutation_gene_count_dict
-
-
-# TODO: this can be unit tested.
-def _process_intergenic_mutation_gene_count(mutation_gene_str, mutation_gene_count_dict):
-    flanking_gene_list = mutation_gene_str.split(INTERGENIC_SPLIT_CHAR)
-    for flanking_gene in flanking_gene_list:
-        mutation_gene_count_dict[flanking_gene] += 1
 
 
 def _get_enrichment_mutation_list(ale_experiment_reseq_mutation_lists,
@@ -49,16 +47,19 @@ def _get_enrichment_mutation_list(ale_experiment_reseq_mutation_lists,
                                   mutation_gene_count_dict):
 
     enrichment_mutation_list = []
-
     for reseq_mutation_list in ale_experiment_reseq_mutation_lists:
-
         for mutation in reseq_mutation_list:
+            if mutation.id not in ignored_mutation_id_list:
 
-            if mutation.id not in ignored_mutation_id_list\
-                    and mutation_gene_count_dict[mutation.gene] > 1\
-                    and mutation not in enrichment_mutation_list:
+                if INTERGENIC_SPLIT_CHAR in mutation.gene:
+                    gene_list = mutation.gene.split(INTERGENIC_SPLIT_CHAR)
+                else:
+                    gene_list = [mutation.gene]
 
-                enrichment_mutation_list.append(mutation)
+                for gene in gene_list:
+                    if mutation_gene_count_dict[gene] > 1 \
+                            and mutation not in enrichment_mutation_list:   # This condition will keep intergenic mutations from being added twice since they consider two genes which may both already have a mutation; in the case the intergenic mutation will only be added once.
+                        enrichment_mutation_list.append(mutation)
 
     return enrichment_mutation_list
 
