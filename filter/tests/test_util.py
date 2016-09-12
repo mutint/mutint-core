4927e27d2efad7fa00467360e98a698658f02d51
pvphaneuf <pphaneuf@eng.ucsd.edu>
2016-09-11 19:28:42 -0700

    added unit tests

--- filter/tests/test_util.py
+++ filter/tests/test_util.py
@@ -46,3 +46,33 @@ class TestEnrichment(TestCase):
                                                              ignored_genes="rcsF, –/–, rrlH, rrlD, rrsH, rrlA, gltP/yjcO, rsxC, eco/mqo")
         obs_mut_queryset = filter_mutations(ObservedMutation.objects.all(), filter_settings)
         self.assertEquals(len(obs_mut_queryset), 0)
+
+    def test_mutation_filter_many_gene_mutation_one_gene_filter(self):
+        mut = Mutation.objects.create(mutation_type="qwe",
+                                      position=1,
+                                      sequence_change="zxcv",
+                                      gene="geneA, geneB")
+        ObservedMutation.objects.create(sequencing_experiment=self.reseq,
+                                        mutation=mut,
+                                        frequency=1)
+
+        filter_settings = AleExperimentFilter.objects.create(ale_experiment=self.ale_exp,
+                                                             ignored_genes="geneA")
+
+        obs_mut_queryset = filter_mutations(ObservedMutation.objects.all(), filter_settings)
+        self.assertEquals(len(obs_mut_queryset), 1)
+
+    def test_mutation_filter_many_gene_mutation_many_gene_filter(self):
+        mut = Mutation.objects.create(mutation_type="qwe",
+                                      position=1,
+                                      sequence_change="zxcv",
+                                      gene="geneA, geneB")
+        ObservedMutation.objects.create(sequencing_experiment=self.reseq,
+                                        mutation=mut,
+                                        frequency=1)
+
+        filter_settings = AleExperimentFilter.objects.create(ale_experiment=self.ale_exp,
+                                                             ignored_genes="geneB, geneA")
+
+        obs_mut_queryset = filter_mutations(ObservedMutation.objects.all(), filter_settings)
+        self.assertEquals(len(obs_mut_queryset), 0)
