4fdee0201832b72ec94b2c850879c02505778fa1
dgosting <dgosting@gmail.com>
2016-09-20 10:44:44 -0700

    fixed gene page table generation

--- seq/util.py
+++ seq/util.py
@@ -1,11 +1,5 @@
 from seq.models import ObservedMutation
 
-from django.core.cache import cache
-
-from genes.util import get_gene_list
-
-from itertools import groupby
-
 
 def get_all_observed_mutations(reseq_id_list):
     return ObservedMutation.objects.filter(sequencing_experiment_id__in=reseq_id_list)
