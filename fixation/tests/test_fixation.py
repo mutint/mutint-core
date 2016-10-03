31f56ab5664b9447c683a38730bca7de792675b9
pvphaneuf <pvphaneuf@gmail.com>
2016-10-02 18:47:37 -0700

    Added test for disappearing fixation

--- fixation/tests/test_fixation.py
+++ fixation/tests/test_fixation.py
@@ -76,9 +76,6 @@ class TestFixation(TestCase):
         self.assertEquals(0, len(fixation.fixating_mutation_list))
 
     def test_fixation_single_mutation(self):
-
-        # only considering one ALE
-
         flask_1 = Flask.objects.create(ale_id=self.ale,
                                        media=self.media,
                                        flask_number=1)
@@ -127,6 +124,78 @@ class TestFixation(TestCase):
         for mutation in fixation.fixating_mutation_list:
             self.assertEquals(expected_fixation_gene, mutation.gene)
 
+    def test_lost_fixation(self):
+        """
+        Mutation fixates, but then disappears.
+        This could happen with replacement mutations that provide more fitness on same gene; we should handle
+        this as a fixation in future implementations.
+        """
+        flask_1 = Flask.objects.create(ale_id=self.ale,
+                                       media=self.media,
+                                       flask_number=1)
+        isolate_1 = Isolate.objects.create(flask=flask_1,
+                                           isolate_number=1,
+                                           is_population=False,
+                                           freezer_box=self.freezer_box)
+        tech_rep_1 = TechnicalReplicate.objects.create(isolate=isolate_1)
+        reseq_1 = ResequencingExperiment.objects.create(tech_rep=tech_rep_1)
+
+        flask_2 = Flask.objects.create(ale_id=self.ale,
+                                       media=self.media,
+                                       flask_number=2)
+        isolate_2 = Isolate.objects.create(flask=flask_2,
+                                           isolate_number=1,
+                                           is_population=False,
+                                           freezer_box=self.freezer_box)
+        tech_rep_2 = TechnicalReplicate.objects.create(isolate=isolate_2)
+        reseq_2 = ResequencingExperiment.objects.create(tech_rep=tech_rep_2)
+
+        flask_3 = Flask.objects.create(ale_id=self.ale,
+                                       media=self.media,
+                                       flask_number=3)
+        isolate_3 = Isolate.objects.create(flask=flask_3,
+                                           isolate_number=1,
+                                           is_population=False,
+                                           freezer_box=self.freezer_box)
+        tech_rep_3 = TechnicalReplicate.objects.create(isolate=isolate_3)
+        reseq_3 = ResequencingExperiment.objects.create(tech_rep=tech_rep_3)
+
+        mut1 = Mutation.objects.create(mutation_type="qwe",
+                                       position=1,
+                                       sequence_change="asdf",
+                                       gene="geneA")
+        ObservedMutation.objects.create(sequencing_experiment=reseq_1,
+                                        frequency=1,
+                                        mutation=mut1)
+
+        # mut1 fixates up to reseq_2, but disappears in reseq_3
+        ObservedMutation.objects.create(sequencing_experiment=reseq_2,
+                                        frequency=1,
+                                        mutation=mut1)
+        mut2 = Mutation.objects.create(mutation_type="qwe",
+                                       position=2,
+                                       sequence_change="asdf",
+                                       gene="geneB")
+        ObservedMutation.objects.create(sequencing_experiment=reseq_2,
+                                        frequency=1,
+                                        mutation=mut2)
+
+        mut3 = Mutation.objects.create(mutation_type="uio",
+                                       position=2,
+                                       sequence_change="wqer",
+                                       gene="geneC")
+        ObservedMutation.objects.create(sequencing_experiment=reseq_3,
+                                        frequency=1,
+                                        mutation=mut3)
+
+        reseq_ordered_dict = collections.OrderedDict()
+        reseq_ordered_dict.update({1: reseq_1})
+        reseq_ordered_dict.update({2: reseq_2})
+        reseq_ordered_dict.update({3: reseq_3})
+        fixation = Fixation(reseq_ordered_dict)
+        self.assertEquals(0, len(fixation.fixating_mutation_list))
+
+    # Test for core internal Fixation logic
     def test_get_ale_fixated_mutation_queryset(self):
 
         # flask_mutation_dict: {flask_id: mutation_query_set}
