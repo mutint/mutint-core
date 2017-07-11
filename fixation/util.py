df3457ebf37103e32d958727230bb577578a3322
pphaneuf <pphaneuf@eng.ucsd.edu>
2017-07-10 17:39:43 -0700

    Removed obsolete comment and function argument.

--- fixation/util.py
+++ fixation/util.py
@@ -7,7 +7,6 @@ __author__ = "Patrick Phaneuf"
 
 
 def get_fixed_mut_dict(ale_experiment_id):
-    filter_settings = get_filter_settings(ale_experiment_id)
-    ale_reseq_ordered_dict = get_reseq_ordered_dict(ale_experiment_id)  # Want to include WT so as to get mutations to remove.
-    ale_exp_fixed_mut_dict = fixation.get_ale_exp_fixed_mut_dict(ale_reseq_ordered_dict, filter_settings)
+    ale_reseq_ordered_dict = get_reseq_ordered_dict(ale_experiment_id)
+    ale_exp_fixed_mut_dict = fixation.get_ale_exp_fixed_mut_dict(ale_reseq_ordered_dict)
     return ale_exp_fixed_mut_dict
