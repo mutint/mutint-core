3e1913a5abb4c0641a85442a584fa148311694b7
pphaneuf <pphaneuf@eng.ucsd.edu>
2017-08-01 13:36:23 -0700

    Removed import of no longer used function.

--- seq/views/mutations.py
+++ seq/views/mutations.py
@@ -4,7 +4,6 @@ from django.utils.safestring import mark_safe
 import seq.views.common
 from seq.views import mutation_table_builder
 from seq.util import get_all_observed_mutations
-import filter.util
 from common.util import get_all_ale_exps, get_recent_ale_exps, get_reseq_ordered_dict
 from common.util import check_hidden_columns_and_filters
 from common.constants import POSITION_COLUMN_IN_REGULAR_MUTATION_TABLE
