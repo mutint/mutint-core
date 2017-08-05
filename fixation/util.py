34a69ef7fa67c7143aeb719f3625ec0e534b92ac
pphaneuf <pphaneuf@eng.ucsd.edu>
2017-08-04 19:47:25 -0700

    Optimized fixed mutation observed mutation fetching.

--- fixation/util.py
+++ fixation/util.py
@@ -18,32 +18,10 @@ def get_exp_fixed_obs_mut_qryset(reseq_ordered_dict):
     exp_id = reseq_ordered_dict[next(iter(reseq_ordered_dict))].ale_experiment.ale_id
     fixed_mut_qryset = FixatedMutation.objects.filter(ale_experiment_id=exp_id)
     fixed_obs_mut_qryset = ObservedMutation.objects.none()
-
     # TODO: filter out mutations from samples that were removed from table.
-
-    # TODO: the below can likely be optimized using the django ORM.
     for fixed_mut in fixed_mut_qryset:
-        fixed_obs_mut_qryset_list = _get_fixed_obs_mut_qryset_list(fixed_mut, reseq_ordered_dict.keys())
-        # Could be fixing in more than 1 ALE.
-        for fixed_obs_mut_qryset in fixed_obs_mut_qryset_list:
-            fixed_obs_mut_qryset = fixed_obs_mut_qryset | fixed_obs_mut_qryset
-
+        fixed_obs_mut_id_2d_list = list(ast.literal_eval(fixed_mut.fixed_observed_mutation_series))
+        # Just need to get all observed mutations; don't care their about their grouping.
+        fixed_obs_mut_id_list = [item for sublist in fixed_obs_mut_id_2d_list for item in sublist]
+        fixed_obs_mut_qryset = fixed_obs_mut_qryset | ObservedMutation.objects.filter(id__in=fixed_obs_mut_id_list)
     return fixed_obs_mut_qryset
-
-
-# def _get_fixating_observed_mutation_queryset(fixating_mutation_queryset, reseq_id_list):
-#     fixating_mutation_id_list = [fixating_mutation.mutation.id for fixating_mutation in fixating_mutation_queryset]
-#     all_observed_mutation_queryset = ObservedMutation.objects.filter(sequencing_experiment_id__in=reseq_id_list)
-#     fixating_observed_mutation_queryset = all_observed_mutation_queryset.filter(mutation_id__in=fixating_mutation_id_list)
-#     return fixating_observed_mutation_queryset
-def _get_fixed_obs_mut_qryset_list(fixed_mut, reseq_id_list):
-    # Getting all bos mut in the reseqs for this exp.
-    all_obs_mut_qryset = ObservedMutation.objects.filter(sequencing_experiment_id__in=reseq_id_list)
-    # Getting all obs mut involved in the fixing sequence.
-    fixed_obs_mut_lists = list(ast.literal_eval(fixed_mut.fixed_observed_mutation_series))
-    obs_mut_queryset_list = []
-    for fixed_obs_mut_list in fixed_obs_mut_lists:
-        obs_mut_queryset = all_obs_mut_qryset.filter(id__in=fixed_obs_mut_list)
-        obs_mut_queryset_list.append(obs_mut_queryset)
-
-    return obs_mut_queryset_list
