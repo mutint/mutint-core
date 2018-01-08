26159bdecff96ced629cdf27cae0d842e429f391
Patrick Phaneuf <pphaneuf@eng.ucsd.edu>
2018-01-08 08:56:35 -0800

    Added back defaults.

--- not-docker-runserver.sh
+++ not-docker-runserver.sh
@@ -2,10 +2,10 @@
 
 export DEBUG=1
 export SEQUENCING_URL=http://sbrg.ucsd.edu
-export MYSQL_DATABASE=ale_db
-export MYSQL_USER=ale
-export MYSQL_PASSWORD=REDACTED-CREDENTIAL
-export MYSQL_HOST=localhost
+export MYSQL_DATABASE=database
+export MYSQL_USER=user
+export MYSQL_PASSWORD=password
+export MYSQL_HOST=db
 export MYSQL_PORT=3306
 export DJANGO_SETTINGS_MODULE=aleinfo.settings_private
 export GOOGLE_ANALYTICS_TAG=REDACTED-CREDENTIAL
