9d613cbb6bc3c9d603213580e04b4bdb28e40a58
Patrick Phaneuf <pphaneuf@eng.ucsd.edu>
2018-03-28 18:11:18 -0700

    Removed all of the font-awesome icons.

--- not-docker-runserver.sh
+++ not-docker-runserver.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
 export DEBUG=1
-export SEQUENCING_URL=http://aledb.org/aledata/
-export MYSQL_DATABASE=database
-export MYSQL_USER=user
-export MYSQL_PASSWORD=password
-export MYSQL_HOST=db_name
+export SEQUENCING_URL=http://sbrg.ucsd.edu
+export MYSQL_DATABASE=ale_db
+export MYSQL_USER=root
+export MYSQL_PASSWORD=REDACTED-CREDENTIAL
+export MYSQL_HOST=localhost
 export MYSQL_PORT=3306
 export DJANGO_SETTINGS_MODULE=aleinfo.settings_private
-export ALE_DATA_ROOT_DIR=/var/www/ale_analytics_data/
-export GOOGLE_ANALYTICS_TAG=REDACTED-CREDENTIAL
+export GOOGLE_ANALYTICS_TAG=XXXX
+export PUBLIC=1
 
 if [ "$DEBUG" = 0 ]
 then
   gunicorn aleinfo.wsgi -b 0.0.0.0:8000 --reload
 else
   echo "!!!DEBUG!!! DEPLOYMENT"
-  python3 manage.py runserver localhost:8000
+  python3 manage.py runserver
 fi
