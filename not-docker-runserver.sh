c3b4ff388c79598ea63dbd2f51144386abc54e74
pvphaneuf <pphaneuf@eng.ucsd.edu>
2018-04-21 21:47:11 +0000

    fixed bug where removed production deployment

--- not-docker-runserver.sh
+++ not-docker-runserver.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 source ./config.sh
-python3 manage.py runserver
+if [ "$DEBUG" = 0 ]
+then
+  gunicorn aleinfo.wsgi -b 0.0.0.0:8000 --reload
+else
+  echo "!!!DEBUG!!! DEPLOYMENT"
+  python3 manage.py runserver
+fi
