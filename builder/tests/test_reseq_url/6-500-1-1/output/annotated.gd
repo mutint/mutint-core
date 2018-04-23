52cdd61d6ab488438dfb594c8e463422aa9db3ba
pvphaneuf <pphaneuf@eng.ucsd.edu>
2018-04-23 18:32:44 +0000

    added unit testing files

---
+++ builder/tests/test_reseq_url/6-500-1-1/output/annotated.gd
@@ -0,0 +1,14 @@
+#=GENOME_DIFF	1.0
+#=TITLE	Ara-6_500gen_767A
+#=AUTHOR	Deatherage, Daniel
+#=TIME	500
+#=POPULATION	Ara-6
+#=TREATMENT	LTEE
+#=CLONE	A
+#=REFSEQ	https://raw.githubusercontent.com/barricklab/LTEE/7da91974eafac0c5a8f903ae57275795d4395af2/reference/REL606.gbk
+#=READSEQ	ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR258/009/SRR2584929/SRR2584929_1.fastq.gz
+#=READSEQ	ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR258/009/SRR2584929/SRR2584929_2.fastq.gz
+#=MUTATOR_STATUS	non-mutator
+MOB	1	.	REL606	241641	IS150	1	3	ambiguous=1	end_position=241643	gene_name=insB-2	gene_position=coding (136-138/504 nt)	gene_product=IS1 protein InsB	gene_strand=>	genes_inactivated=insB-2	html_gene_name=<i>insB&#8209;2</i>&nbsp;&rarr;	html_gene_product=IS1 protein InsB	html_mutation=IS<i>150</i>&nbsp;(+)&nbsp;+3&nbsp;bp	html_mutation_annotation=coding&nbsp;(136&#8209;138/504&nbsp;nt)	html_position=241,641	html_seq_id=REL606	locus_tag=ECB_00211	locus_tags_inactivated=ECB_00211	mutation_category=mobile_element_insertion	phylogeny_id=1	start_position=241641
+SNP	2	.	REL606	1329493	C	aa_new_seq=S	aa_position=25	aa_ref_seq=Y	codon_new_seq=TCC	codon_number=25	codon_position=2	codon_ref_seq=TAC	end_position=1329493	gene_name=topA	gene_position=74	gene_product=DNA topoisomerase I	gene_strand=>	genes_overlapping=topA	html_gene_name=<i>topA</i>&nbsp;&rarr;	html_gene_product=DNA topoisomerase I	html_mutation=A&rarr;C	html_mutation_annotation=<font class="snp_type_nonsynonymous">Y25S</font>&nbsp;(T<font class="mutation_in_codon">A</font>C&rarr;T<font class="mutation_in_codon">C</font>C)&nbsp;	html_position=1,329,493	html_seq_id=REL606	locus_tag=ECB_01250	locus_tags_overlapping=ECB_01250	mutation_category=snp_nonsynonymous	snp_type=nonsynonymous	start_position=1329493	transl_table=11
+DEL	3	.	REL606	3894997	7813	end_position=3902809	gene_name=rbsD–[yieP]	gene_product=rbsD, rbsA, rbsC, rbsB, rbsK, rbsR, yieO, [yieP]	genes_inactivated=rbsD,rbsA,rbsC,rbsB,rbsK,rbsR,yieO,yieP	html_gene_name=<i>rbsD</i>&#8211;<i>[yieP]</i>	html_gene_product=<i>rbsD, rbsA, rbsC, rbsB, rbsK, rbsR, yieO, [yieP]</i>	html_mutation=&Delta;7,813&nbsp;bp	html_mutation_annotation=IS<i>150</i>&#8209;mediated	html_position=3,894,997	html_seq_id=REL606	locus_tag=[ECB_03634]–[ECB_03641]	locus_tags_inactivated=ECB_03634,ECB_03635,ECB_03636,ECB_03637,ECB_03638,ECB_03639,ECB_03640,ECB_03641	mediated=IS150	mutation_category=large_deletion	start_position=3894997
