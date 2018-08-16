26ecf690d7eeab645036e694c2b0f5bba11b94dc
Muyao Wu <muyao@muyao.ucsd.edu>
2018-08-16 13:47:47 -0700

    using a formatter

--- logs/aledb_logger.py
+++ logs/aledb_logger.py
@@ -9,7 +9,7 @@ LOGGING = {
     'disable_existing_loggers': True,
     'formatters': {
         'standard': {
-            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'format' : "logging things [%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
             'datefmt' : "%d/%b/%Y %H:%M:%S"
         },
     },
