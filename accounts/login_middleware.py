900ff4d936767db0b58ce2486d9c98a06e360f1c
Muyao Wu <muyao@muyao.ucsd.edu>
2018-08-20 15:24:09 -0700

    trying differnt way to get user info

--- accounts/login_middleware.py
+++ accounts/login_middleware.py
@@ -9,6 +9,7 @@ if hasattr(settings, 'LOGIN_EXEMPT_URLS'):
 
 
 class LoginRequiredMiddleware:
+
     """
     Middleware that requires a user to be authenticated to view any page other
     than LOGIN_URL. Exemptions to this requirement can optionally be specified
@@ -19,19 +20,17 @@ class LoginRequiredMiddleware:
     """
 
     def process_request(self, request):
-
-
-
+        log = getLogger(__name__)
         assert hasattr(request, 'user'), "The Login Required middleware\
  requires authentication middleware to be installed. Edit your\
  MIDDLEWARE_CLASSES setting to insert\
  'django.contrib.auth.middleware.AuthenticationMiddleware'. If that doesn't\
  work, ensure your TEMPLATE_CONTEXT_PROCESSORS setting includes\
  'django.core.context_processors.auth'."
-
         if not request.user.is_authenticated():
             path = request.path_info.lstrip('/')
             if not any(m.match(path) for m in EXEMPT_URLS):
+                log.warning("User not log in, redirecting to log-in page", extra=getUserExtras(request))
                 return HttpResponseRedirect(settings.LOGIN_URL)
-        log = getLogger(__name__)
-        log.info("User Logged In", extra = getUserExtras(request))
+
+        log.warning("User Logged In", extra = getUserExtras(request))
