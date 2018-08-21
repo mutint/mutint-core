9237c80c212bdb8d328ad3eca749a2dc0e7a54d9
Muyao Wu <muyao@muyaos-imac.dynamic.ucsd.edu>
2018-08-21 14:02:35 -0700

    let's see if our logger will catch all exceptions

--- logs/aledb_logger.py
+++ logs/aledb_logger.py
@@ -104,7 +104,7 @@ def handle_exception(exc_type, exc_value, exc_traceback):
         sys.__excepthook__(exc_type, exc_value, exc_traceback)
         return
 
-    log.error("Uncaught exception", exc_info=(exc_type, exc_value, exc_traceback))
+    log.exception("Uncaught exception", exc_info=(exc_type, exc_value, exc_traceback))
 
 
 sys.excepthook = handle_exception
