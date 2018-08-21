74a6013d95fad8e3d8c5969414b4890db565db1f
Muyao Wu <muyao@muyao.ucsd.edu>
2018-08-21 10:10:47 -0700

    trying differnt way to get user info

--- logs/aledb_logger.py
+++ logs/aledb_logger.py
@@ -68,13 +68,13 @@ def getLogger(logname = None):
 def get_client_ip(request):
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
     if x_forwarded_for:
-        print "returning FORWARDED_FOR"
+        print ("returning FORWARDED_FOR")
         ip = x_forwarded_for.split(',')[-1].strip()
     elif request.META.get('HTTP_X_REAL_IP'):
-        print "returning REAL_IP"
+        print ("returning REAL_IP")
         ip = request.META.get('HTTP_X_REAL_IP')
     else:
-        print "returning REMOTE_ADDR"
+        print ("returning REMOTE_ADDR")
         ip = request.META.get('REMOTE_ADDR')
     return ip
 
