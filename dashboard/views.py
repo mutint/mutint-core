1db712dbc19c561ad5a003215356e74774ac125f
Muyao Wu <muyao@muyao.ucsd.edu>
2018-08-22 11:17:41 -0700

    let's see if our logger will catch all exceptions

--- dashboard/views.py
+++ dashboard/views.py
@@ -10,7 +10,7 @@ from dashboard.timeline_util import get_timeline
 from stats.views import get_histogram_item_count
 from dashboard.models import BarCharts
 from stats.util import MAX_HISTOGRAM_SIZE
-from logs.aledb_logger import UserLoggingAdaptor,getLogger,get_client_ip,getUserExtras
+from logs.aledb_logger import getLogger,getUserExtras
 
 DEFAULT_IGNORED_MUTATIONS = "[]"
 DASHBOARD_TEMPLATE = "dashboard.html"
@@ -22,8 +22,6 @@ def dashboard(request):
     log.info("populating dashboard with log", extra = getUserExtras(request))
 
     try:
-
-        print(2/0)
 
         general_count_dict = _get_general_count_dict()
         observed_mutation_counts = ObservedMutationCounts.objects.first()
@@ -66,7 +64,6 @@ def dashboard(request):
 
     except Exception as e:
         log.exception(e, extra = getUserExtras(request))
-        return HttpResponse("something broke")
 
 
 def _get_general_count_dict():
