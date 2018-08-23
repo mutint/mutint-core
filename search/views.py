4c0bf9277a90a7d829cb98763361f37a228260d1
Muyao Wu <muyao@muyao.ucsd.edu>
2018-08-23 14:48:45 -0700

    putting example of util logging search weith context

--- search/views.py
+++ search/views.py
@@ -20,7 +20,8 @@ log = get_logger("aledbLogger")
 
 
 def search(request):
-    log.info("search", extra = user_extra(request))
+
+    log.info("search terms", extra=all_get_extra(request))
     check_hidden_columns_and_filters(request, None)
     obs_mut_qryset = _get_obs_mut_qryset(request)
     reseq_dict = _get_ordered_reseq_dict(obs_mut_qryset)
@@ -45,7 +46,7 @@ def search(request):
                "experiments": get_all_ale_exps(),
                "recent_experiments": get_recent_ale_exps()}
 
-    log.info("search terms", extra = all_get_extra(request))
+
 
     return HttpResponse(template.render(context, request), content_type="text/html")
 
