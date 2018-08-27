b80eaab9014a88fa0da40cb0dbf844bdca091359
Muyao Wu <muyao@muyaos-imac.dynamic.ucsd.edu>
2018-08-27 09:56:25 -0700

    removing print statements

--- logs/aledb_logger.py
+++ logs/aledb_logger.py
@@ -96,13 +96,10 @@ def get_logger(logname=None):
 def get_client_ip(request):
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
     if x_forwarded_for:
-        print ("returning FORWARDED_FOR")
         ip = x_forwarded_for.split(',')[-1].strip()
     elif request.META.get('HTTP_X_REAL_IP'):
-        print ("returning REAL_IP")
         ip = request.META.get('HTTP_X_REAL_IP')
     else:
-        print ("returning REMOTE_ADDR")
         ip = request.META.get('REMOTE_ADDR')
     return ip
 
