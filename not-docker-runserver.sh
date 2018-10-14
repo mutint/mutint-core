05396909dac7b798b9b6687a868d16277bc6bf3a
pphaneuf <pphaneuf@eng.ucsd.edu>
2018-10-14 11:56:24 -0700

    ASW-136: increasing gunicorn timeout to 1000 sec within ./not-docker-runserver.sh

--- not-docker-runserver.sh
+++ not-docker-runserver.sh
@@ -5,7 +5,7 @@ if [ "$DEBUG" = 0 ]
 then
   python3 manage.py migrate
   python3 manage.py collectstatic --no-input
-  gunicorn aleinfo.wsgi -b 0.0.0.0:8000 --reload
+  gunicorn aleinfo.wsgi -t 1000 -b 0.0.0.0:8000 --reload
 else
   echo "!!!DEBUG!!! DEPLOYMENT"
   python3 manage.py migrate
