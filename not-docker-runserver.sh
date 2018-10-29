a8e9bde1f90ffed7d78e5272be148348938f099c
pphaneuf <pphaneuf@eng.ucsd.edu>
2018-10-29 08:05:38 -0700

    removed migration execution from none-docker deployment

--- not-docker-runserver.sh
+++ not-docker-runserver.sh
@@ -3,12 +3,10 @@
 source ./config.sh
 if [ "$DEBUG" = 0 ]
 then
-  python3 manage.py migrate
   python3 manage.py collectstatic --no-input
   gunicorn aleinfo.wsgi -t 1000 -b 0.0.0.0:8000 --reload
 else
   echo "!!!DEBUG!!! DEPLOYMENT"
-  python3 manage.py migrate
   python3 manage.py collectstatic --no-input
   python3 manage.py runserver
 fi
