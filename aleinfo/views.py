b95cd167f339b23e61f235ff9e237ea069a0eebc
Robin Cai <robincai92130@gmail.com>
2019-03-04 16:23:36 -0800

    ASW-721 fix broken links

--- aleinfo/views.py
+++ aleinfo/views.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.http import HttpResponse, Http404, HttpResponseForbidden
 from ale.permissions import can_view_experiment
 import logging
-import os
+import os, io
 
 DOC_ROOT = settings.ALE_DATA_ROOT_DIR
 
@@ -10,22 +10,32 @@ logger = logging.getLogger(__name__)
 
 
 def protected_file_serve(request, page_name: str):
+    """
+    User can only view files in output folder. Make sure to handle files with binary data, e.g. images
+    :param request:
+    :param page_name:
+    :return: the requested file or error if no permission or file link not available
+    """
     user = request.user
-    if len(page_name) > 0 and '/' in page_name:
-        slash_loc = page_name.rfind('/')
-        reseq_location = page_name[0:slash_loc+1]
+    if len(page_name) > 0 and 'output/' in page_name:
+        if page_name.endswith('output/'):
+            reseq_location = page_name
+        else:
+            output_loc = page_name.find('output/')
+            reseq_location = page_name[0:output_loc+len('output/')]
         ok = can_view_experiment(user, reseq_location)
         if not ok:
             logger.error("file path error: " + "Cannot view the link " + page_name)
             raise HttpResponseForbidden
-    if page_name.endswith('/'):
-        page_name = page_name + "index.html"
-    logger.info("display file " + page_name)
-    file_path = DOC_ROOT + page_name
-    if os.path.isfile(file_path):
-        with open(DOC_ROOT + page_name) as f:
-            response = HttpResponse(f.read())
-        return response
+        if page_name.endswith('/'):
+            page_name = page_name + "index.html"
+        logger.info("display file " + page_name)
+        file_path = DOC_ROOT + page_name
+        if os.path.isfile(file_path):
+            with open(DOC_ROOT + page_name, 'rb') as f:
+                response = HttpResponse(f.read())
+            return response
     else:
         logger.error("file path error: " + "page not available - " + page_name)
         raise Http404
+
