2c0bbaddc28c9ba779572907b68cd8c8cf3a3931
wumuyao1996 <wumuyao1996@gmail.com>
2019-03-27 14:57:35 -0700

    Upload reference genomes to mutations

--- builder/upload.py
+++ builder/upload.py
@@ -1,6 +1,8 @@
 from os.path import join
 import re
 from bs4 import BeautifulSoup
+
+from ale.models import AleExperiment, Isolate, TechnicalReplicate
 from builder.gdparse.gdparse import gdparse
 import collections
 import numbers
@@ -13,6 +15,7 @@ from genes.util import get_annotated_gene_list
 from filter.models import AleExperimentFilter
 import filter.models
 from django.conf import settings
+from seq.util import get_ordered_reseq_queryset
 
 
 HTML_SUMMARY_FILE_NAME = "summary.html"
@@ -28,6 +31,7 @@ GD_MUT_TYPE_ATTR_KEY = 'type'
 GD_MUT_FREQ_ATTR_KEY = 'frequency'
 GD_MUT_HTML = 'html_mutation'
 GD_MUT_ANNOTATION_HTML = "html_mutation_annotation"
+GD_MUT_SEQ_ID_ATTR_KEY = 'seq_id'
 DEFAULT_CLONAL_FREQ = 1
 BRESEQ_REPORT_COLUMN_KEY_EVIDENCE = "evidence"
 
@@ -100,11 +104,11 @@ def _database_unassigned_missing_coverage(seq_experiment, evidence_dict, breseq_
         missing_coverage_dict[position] = [attrs[0].find("a")['href'],  # reads_left_url
                                            attrs[1].find("a")['href'],  # reads_right_url
                                            attrs[2].find("a")['href'],  # coverage
-                                           attrs[6].get_text(),         # size
-                                           attrs[7].get_text(),         # reads_left
-                                           attrs[8].get_text(),         # reads_right
-                                           attrs[9].get_text(),         # gene
-                                           attrs[10].get_text()]        # description
+                                           attrs[6].get_text(),  # size
+                                           attrs[7].get_text(),  # reads_left
+                                           attrs[8].get_text(),  # reads_right
+                                           attrs[9].get_text(),  # gene
+                                           attrs[10].get_text()]  # description
     for key in evidence_dict:
         if _is_missing_coverage_type(evidence_dict[key]):
             # TODO: make literals into constants
@@ -148,8 +152,8 @@ def _get_reseq_experiment_with_stats(breseq_folder, technical_replicate_id, pers
     if os.path.isfile(index_file_path):
         breseq_path = breseq_folder[breseq_folder.find(ale_data_root_dir) + len(ale_data_root_dir):]
     reseq, created = ResequencingExperiment.objects.get_or_create(location=breseq_path,
-            tech_rep_id=technical_replicate_id,
-            person=person)
+                                                                  tech_rep_id=technical_replicate_id,
+                                                                  person=person)
     statistics_html = _get_beautifulsoup_html(breseq_folder, HTML_SUMMARY_FILE_NAME)
     if statistics_html:
         row_read_info = statistics_html.find("tr", attrs={"class": "highlight_table_row"}).findChildren("td")
@@ -203,6 +207,7 @@ def _database_mutations(sample_type,
         mut, \
         created = Mutation.objects.get_or_create(position=mutation_dict[mut_num].get(GD_MUT_POS_ATTR_KEY),
                                                  gene=gene_list_str,
+                                                 reseq_reference=mutation_dict[mut_num].get(GD_MUT_SEQ_ID_ATTR_KEY),
                                                  product=breseq_gene_product_annotation,
                                                  sequence_change=sequence_change_str,
                                                  mutation_type=mutation_dict[mut_num].get(GD_MUT_TYPE_ATTR_KEY),
@@ -229,8 +234,8 @@ def _database_mutations(sample_type,
 
     if is_wild_type is True:
         exp_filter, created = AleExperimentFilter.objects.get_or_create(
-                ale_experiment=experiment,
-                defaults=filter.models.get_default_experiment_filter_params(experiment))
+            ale_experiment=experiment,
+            defaults=filter.models.get_default_experiment_filter_params(experiment))
         exp_filter.starting_strain_mutations = ','.join(str(mut) for mut in wild_type_mutation_list)
         exp_filter.save()
 
@@ -271,7 +276,6 @@ def _get_html_mutations_resultset(mutations_html, sample_type):
 
 
 def _get_unassigned_missing_coverage_rows(mutations_html):
-
     try:
         mutation_table = mutations_html.find("th", attrs={"class": "missing_coverage_header_row"}).parent.parent
 
@@ -289,3 +293,27 @@ def _find_between(s, first, last):
         return s[start:end]
     except ValueError:
         return ""
+
+
+def upload_references(ale_id):
+    # first, find a way to get to annotated.gd using the aleid. The experiment root path is reseq.location
+    reseq_experiments = get_ordered_reseq_queryset(ale_id)
+    if hasattr(settings, "ALE_DATA_ROOT_DIR"):
+        reseq_report_url = settings.ALE_DATA_ROOT_DIR
+    else:
+        print("sequencing_url not found")
+    for reseq_experiment in reseq_experiments:
+        path = os.path.join(reseq_report_url, reseq_experiment.location + "annotated.gd")
+        if os.path.exists(path):
+            print(path)
+        else:
+            print("path not found:", path)
+    all_observed_mutations = ObservedMutation.objects.all()
+    ctr = len(all_observed_mutations)
+    for observed_mutation in all_observed_mutations:
+        mutation = Mutation.objects.get(id=observed_mutation.mutation_id)
+        reseq_experiment = observed_mutation.sequencing_experiment
+        print(observed_mutation.id, ctr-1)
+        ctr -=1
+        mutation.reseq_reference = str(Isolate.objects.get(id=TechnicalReplicate.objects.get(id=reseq_experiment.tech_rep_id).isolate_id).reseq_reference).replace(".gbk","").replace(".gb")
+        mutation.save()
