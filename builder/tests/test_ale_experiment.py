bf206bf2894477fa3f1cf6df74b727cf501d4dd4
wumuyao1996 <wumuyao1996@gmail.com>
2019-04-02 10:27:09 -0700

    updating metadata files

--- builder/tests/test_ale_experiment.py
+++ builder/tests/test_ale_experiment.py
@@ -1,12 +1,12 @@
 from django.test import TestCase
-# from dashboard.models import ObservedMutationCounts, UniqueMutationCount
+from django.contrib.auth.models import User
 from seq.models import Mutation, ResequencingExperiment
 from builder.ale_experiment import rebuild_dashboard_data, create_ale_experiment
+from datetime import datetime
 import os
 
 
 class TestEnrichment(TestCase):
-
     # TODO: the unit test below no longer works: need to find out why.
     """
     def test_rebuild_counts_only_builds_one(self):
@@ -20,14 +20,20 @@ class TestEnrichment(TestCase):
         self.assertEqual(UniqueMutationCounts.objects.count(), 1)
     """
 
+    def setUp(self):
+        print("Creating test user Patrick")
+        User.objects.create(username="Patrick", password="test123",
+                            first_name="Patrick", last_name="Phaneuf", email="email@email.com",
+                            is_active=True, is_staff=True, date_joined=datetime.now())
+
     def test_create_ALE_experiment(self):
-        test_report_path = os.path.dirname(os.path.realpath(__file__))+"/breseq/"
+        test_report_path = os.path.dirname(os.path.realpath(__file__)) + "/breseq/"
         create_ale_experiment(test_report_path, "Patrick", "test", "test_project")
         expected_mutation_count = 27
         self.assertEqual(expected_mutation_count, Mutation.objects.all().count())
 
     def test_reseq_URL(self):
-        test_report_path = os.path.dirname(os.path.realpath(__file__))+"/test_reseq_url/"
+        test_report_path = os.path.dirname(os.path.realpath(__file__)) + "/test_reseq_url/"
         create_ale_experiment(test_report_path, "Patrick", "test", "test_project")
         reseq_qryset = ResequencingExperiment.objects.all()
         empty_count = 0
@@ -39,4 +45,3 @@ class TestEnrichment(TestCase):
                 not_empty_count += 1
         self.assertEqual(empty_count, 1)
         self.assertEqual(not_empty_count, 1)
-
