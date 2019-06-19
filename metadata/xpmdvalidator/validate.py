1165f4453207a54a7bbc996e7b931f79d72fff2a
wumuyao1996 <wumuyao1996@gmail.com>
2019-06-19 12:04:24 -0700

    Adding log statement to catch csv crashes

--- metadata/xpmdvalidator/validate.py
+++ metadata/xpmdvalidator/validate.py
@@ -5,7 +5,9 @@ import csv
 import os
 import json
 from jsonschema import Draft4Validator
+import logging
 
+logger = logging.getLogger(__name__)
 def is_valid(csv_directory_path, schema_file_path):
 
     Valdation = True
@@ -15,49 +17,51 @@ def is_valid(csv_directory_path, schema_file_path):
 
     for filename in os.listdir(csv_directory_path):
         if filename.endswith(".csv"):
+            try:
+                csvfile = os.path.splitext(filename)[0]
 
-            csvfile = os.path.splitext(filename)[0]
+                transpose = zip(*csv.reader(open(csv_directory_path + "/" + csvfile +'.csv', "r")))
+                csv.writer(open("transposedFile.csv", "w")).writerows(transpose)
 
-            transpose = zip(*csv.reader(open(csv_directory_path + "/" + csvfile +'.csv', "r")))
-            csv.writer(open("transposedFile.csv", "w")).writerows(transpose)
+                with open("transposedFile.csv") as f:
+                    reader = csv.DictReader(f)
+                    rows = list(reader)
 
-            with open("transposedFile.csv") as f:
-                reader = csv.DictReader(f)
-                rows = list(reader)
+                jsonobj = json.dumps(rows)
+                jsonarray = json.loads(jsonobj)
 
-            jsonobj = json.dumps(rows)
-            jsonarray = json.loads(jsonobj)
+                validator = Draft4Validator(schema)
 
-            validator = Draft4Validator(schema)
+                if (len(sorted(validator.iter_errors(jsonarray), key=str))) > 0:
+                    Valdation = False
 
-            if (len(sorted(validator.iter_errors(jsonarray), key=str))) > 0:
-                Valdation = False
+                    print("ERROR - Metadata File " + filename + " Does not Pass Valdation. Error Logfile generated.")
 
-                print("ERROR - Metadata File " + filename + " Does not Pass Valdation. Error Logfile generated.")
+                    i=0
 
-                i=0
+                    print("Please fix all errors below for Metadata File " + filename + " in order to run file on ALE mutation Pipeline")
 
-                print("Please fix all errors below for Metadata File " + filename + " in order to run file on ALE mutation Pipeline")
+                    for error in sorted(validator.iter_errors(jsonarray), key=str):
+                        i+=1
+                        if error.validator == "pattern":
+                            if (not (not (error.path[1] == 'ALE-number') and not (error.path[1] == 'Flask-number') and not (
+                                    error.path[1] == 'Isolate-number') and not (
+                                    error.path[1] == 'technical-replicate-number')) or (error.path[1] == 'sample-time') or (error.path[1] == "temperature(Celcius")):
+                                print('['+str(i)+'] - ' + 'Invalid input, numerical value only for field: ' + error.path[1])
+                            else:
+                                print('['+str(i)+'] - ' +'Invalid input for field: ' + error.path[1])
 
-                for error in sorted(validator.iter_errors(jsonarray), key=str):
-                    i+=1
-                    if error.validator == "pattern":
-                        if (not (not (error.path[1] == 'ALE-number') and not (error.path[1] == 'Flask-number') and not (
-                                error.path[1] == 'Isolate-number') and not (
-                                error.path[1] == 'technical-replicate-number')) or (error.path[1] == 'sample-time') or (error.path[1] == "temperature(Celcius")):
-                            print('['+str(i)+'] - ' + 'Invalid input, numerical value only for field: ' + error.path[1])
-                        else:
-                            print('['+str(i)+'] - ' +'Invalid input for field: ' + error.path[1])
+                        if error.validator == "enum":
+                            print('['+str(i)+'] - Field: ' + str(error.path[1]) + ", " + error.message)
 
-                    if error.validator == "enum":
-                        print('['+str(i)+'] - Field: ' + str(error.path[1]) + ", " + error.message)
+                        if error.validator == "required":
+                            print('['+str(i)+'] - ' + error.message)
 
-                    if error.validator == "required":
-                        print('['+str(i)+'] - ' + error.message)
+                        elif (error.validator == "required") and (error.validator == "enum") and (error.validator == "pattern"):
+                            print('['+str(i)+'] - ' + error.message)
+            except Exception as e:
+                logger.exception("metadata faulty", extras={"metadata path": filename})
 
-                    elif (error.validator == "required") and (error.validator == "enum") and (error.validator == "pattern"):
-                        print('['+str(i)+'] - ' + error.message)
-     
     try:
         os.remove("transposedFile.csv")
     except OSError:
