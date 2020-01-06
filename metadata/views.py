ba75df9ad0f66d95ae5fe875ff174ee84af28c42
wumuyao1996 <wumuyao1996@gmail.com>
2020-01-06 12:55:46 -0800

    ASW-1481-calcium-source

--- metadata/views.py
+++ metadata/views.py
@@ -78,6 +78,7 @@ def get_reseq_info_list(reseq_queryset):
                                  reseq.tech_rep.isolate.flask.media.nitrogen_source,
                                  reseq.tech_rep.isolate.flask.media.phosphorus_source,
                                  reseq.tech_rep.isolate.flask.media.sulfur_source,
+                                 reseq.tech_rep.isolate.flask.media.carbon_source,
                                  reseq.tech_rep.isolate.flask.media.supplement,
                                  reseq.tech_rep.isolate.flask.media.temperature,
                                  reseq.tech_rep.isolate.flask.ale_id.strain,
