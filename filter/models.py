6966d5105c311b1a20baf810bce49c104999847a
wumuyao1996 <wumuyao1996@gmail.com>
2020-02-28 13:49:44 -0800

    increasing filters to 15000

--- filter/models.py
+++ filter/models.py
@@ -22,11 +22,11 @@ class AleExperimentFilter(models.Model):
     min_cutoff = models.PositiveSmallIntegerField(default=DEFAULT_MUTATION_FREQ_MIN)  # TODO: this should like rather be a decimal to it's conterpart of seq.models.ObservedMutation.frequency
     max_cutoff = models.PositiveSmallIntegerField(default=DEFAULT_MUTATION_FREQ_MAX)  # TODO: this should like rather be a decimal to it's conterpart of seq.models.ObservedMutation.frequency
     ignored_genes = models.TextField(default='', blank=True)
-    ignored_mutations = models.CharField(max_length=500, default='', blank=True)
-    starting_strain_mutations = models.CharField(max_length=500, default='', blank=True)
+    ignored_mutations = models.CharField(max_length=15000, default='', blank=True)
+    starting_strain_mutations = models.CharField(max_length=15000, default='', blank=True)
 
 
 class GlobalFilter(models.Model):
 
-    ignored_genes = models.CharField(max_length=500, default='', blank=True)
-    ignored_mutations = models.CharField(max_length=500, default='', blank=True)
+    ignored_genes = models.CharField(max_length=15000, default='', blank=True)
+    ignored_mutations = models.CharField(max_length=15000, default='', blank=True)
