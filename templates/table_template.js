1864783de6424723f3713855bca2960206fa6f7f
wumuyao1996 <wumuyao1996@gmail.com>
2021-11-23 00:32:08 -0800

    removing the confirmation on filter addition

--- templates/table_template.js
+++ templates/table_template.js
@@ -343,15 +343,6 @@ function save_to_experiment_filter(ale_experiment_id, mutation_id) {
                 experiment_id: ale_experiment_id,
                 save_method: "experiment"
             },
-            success: function (result) {
-                var message = result['content'];
-                if (message == 'ok') {
-                    swal("", "Added mutation to experiment filter", "success");
-                }
-                else {
-                    swal("", message, "info")
-                }
-            },
             error: function (xhr, ajaxOptions, thrownError) {
                 swal("", "Save-tag failed", "error");
             }
