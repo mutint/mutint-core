43e0115d951deaa6f27607ad9fcd740e576e5355
wumuyao1996 <muw006@ucsd.edu>
2021-12-16 07:39:45 -0800

    separate filter button

--- seq/views/mutation_table_builder.py
+++ seq/views/mutation_table_builder.py
@@ -60,10 +60,10 @@ def _build_table_cell_for_dropdown(mutation, ale_experiment):
 
     # some other tables have a 'Save to Experiment Filter' menuitem
     if ale_experiment:
-        filter_button = _button_save_to_experiment_filter % (ale_experiment.ale_id, mutation.id)
+        filter_button += _button_save_to_experiment_filter % (ale_experiment.ale_id, mutation.id)
         menuitems += _menu_item_save_to_experiment_filter % (ale_experiment.ale_id, mutation.id)
 
-    return filter_button+str(_table_cell_dropdown_template % (menuitems + _get_tag_filter_dropdown_entries(mutation.id)))
+    return filter_button+_table_cell_dropdown_template % (menuitems + _get_tag_filter_dropdown_entries(mutation.id))
 
 
 class TableType(Enum):
