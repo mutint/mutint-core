925bcb8e906dd2748442f47d9ddbdb41dfbf08d3
wumuyao1996 <muw006@ucsd.edu>
2021-12-15 23:12:07 -0800

    adding filter admin page

---
+++ filter/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django import forms
+from filter.models import AleExperimentFilter
+from guardian.admin import GuardedModelAdmin
+
+
+@admin.register(AleExperimentFilter)
+class ProjectAdmin(GuardedModelAdmin):
+    list_display = ('id', 'ale_experiment', 'min_cutoff', 'max_cutoff')
+    search_fields = ('id', 'ale_experiment')
