729849dd76b6352530b556f0ea3592ea01063490
wumuyao1996 <muw006@ucsd.edu>
2021-12-21 01:10:59 -0800

    filter button updates

--- seq/views/mutation_table_builder.py
+++ seq/views/mutation_table_builder.py
@@ -31,12 +31,13 @@ REP_DROPDOWN = '<div class="dropdown tag_dropdown"><button class="btn btn-defaul
              '</ul>'
 REP_TAG = '</div><div class="tag_dropdown">%s</div>'
 
+_button_save_to_experiment_filter = """<button class="btn btn-default btn-xs type="button" id="experiment_filter_button" onclick="save_to_experiment_filter(%d, %d); return false;"><i class="fa fa-filter" aria-hidden="true"></i></button>"""
 # the dropdown cell in the mutation table
 _menu_item_save_to_global_filter = """<li><a onclick="save_to_global_filter(%d)" style="cursor:pointer">Save to Global Filter</a></li>"""
 _menu_item_save_to_experiment_filter = """<li><a onclick="save_to_experiment_filter(%d, %d)" style="cursor:pointer">Save to Experiment Filter</a></li>"""
 _table_cell_dropdown_template = """<div class="dropdown">
   <button class="btn btn-default btn-xs dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-    <i class="fa fa-filter" aria-hidden="true"></i>
+    <i class="fa fa-bars" aria-hidden="true"></i>
   </button>
   <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
     %s
@@ -51,16 +52,17 @@ def _build_table_cell_for_dropdown(mutation, ale_experiment):
     NOTE: this is a temporary solution for implementing #425 until the
     mutation table component rendering is refactored
     """
-
     menuitems = ''
+    filter_button = ''
     # all tables have a 'Save to Global Filter' menuitem
-    # menuitems = _menu_item_save_to_global_filter % (mutation.id)
+    #menuitems = _menu_item_save_to_global_filter % (mutation.id)
 
     # some other tables have a 'Save to Experiment Filter' menuitem
     if ale_experiment:
+        filter_button += _button_save_to_experiment_filter % (ale_experiment.ale_id, mutation.id)
         menuitems += _menu_item_save_to_experiment_filter % (ale_experiment.ale_id, mutation.id)
 
-    return _table_cell_dropdown_template % (menuitems + _get_tag_filter_dropdown_entries(mutation.id))
+    return filter_button+_table_cell_dropdown_template % (menuitems + _get_tag_filter_dropdown_entries(mutation.id))
 
 
 class TableType(Enum):
