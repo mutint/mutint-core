1a348d3ad2fa90c0de0a3598c161a09135536927
wumuyao1996 <wumuyao1996@gmail.com>
2023-01-19 04:33:54 -0800

    evidence

--- evidence/views.py
+++ evidence/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-
+from ale.permissions import can_view_experiment
 from common.util import get_user_context
 from django.template import loader
 from logs.aledb_logger import user_extra
@@ -13,6 +13,7 @@ logger = logging.getLogger(__name__)
 
 def evidence(request, *args, **kwargs):
     request_details = str(request)
+
     logger.info("evidence request " + request_details, extra=user_extra(request))
 
     evidence_location = '/data' + request.GET.get('location') #kwargs['evidence_location']
--- seq/views/mutation_table_builder.py
+++ seq/views/mutation_table_builder.py
@@ -252,7 +252,8 @@ def _get_table_mutation_entry(observed_mutation, experiment_url_dict, gatk_url_d
             evidence_url = url + _find_between(observed_mutation.evidence, "\"", "\"")
             gatk_url = gatk_url_dict[observed_mutation.sequencing_experiment_id]
             gatk_raw_file_loc = 'details?location=' + gatk_url +'evidence/'+ str(observed_mutation.mutation.position) + '.html'
-            gatk_evidence = gatk_raw_file_loc
+            gatk_detail_url = 'details?mutation='+observed_mutation.id
+            gatk_evidence = gatk_detail_url
 
             if observed_mutation.mutation.feature_length is None:
                 feature_length = 0
