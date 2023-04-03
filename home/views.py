977a786428eb25c31d2478927da0544d3ff3a0ea
wumuyao1996 <muw006@ucsd.edu>
2023-04-03 11:53:14 -0700

    dynamic pub count

--- home/views.py
+++ home/views.py
@@ -6,19 +6,25 @@ from django.template import loader
 from logs.aledb_logger import user_extra
 import logging
 
+from ale.models import AleExperiment
+
 
 logger = logging.getLogger(__name__)
 
+
+def get_unique_publication_count():
+    return AleExperiment.objects.order_by().values('doi').distinct().len()
 
 def home(request):
 
     # TODO: use the template location described within settings.py
 
     logger.info("home", extra=user_extra(request))
-
+    context = get_user_context(request.user)
+    context.update({"unique_publication_count": get_unique_publication_count})
     try:
         template = loader.get_template("home/index.html")
 
-        return HttpResponse(template.render(get_user_context(request.user), request), content_type="text/html")
+        return HttpResponse(template.render(context, request), content_type="text/html")
     except Exception:
         logger.exception("home broke", extra=user_extra(request))
