abdb6b867af24991da9d8af6d928ce94c9c307c7
wumuyao1996 <wumuyao1996@gmail.com>
2023-06-14 09:23:38 -0700

    pipeline

--- pipeline/views.py
+++ pipeline/views.py
@@ -1,5 +1,3 @@
-from django.shortcuts import render
-
 # Create your views here.
 from django.http import HttpResponse
 
@@ -7,3 +5,22 @@ from common.util import get_user_context
 
 from django.template import loader
 from logs.aledb_logger import user_extra
+
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+def pipeline(request):
+
+    # TODO: use the template location described within settings.py
+
+    logger.info("pipeline", extra=user_extra(request))
+
+    try:
+        template = loader.get_template("pipeline/pipeline.html")
+
+        return HttpResponse(template.render(get_user_context(request.user), request), content_type="text/html")
+    except Exception:
+        logger.exception("pipeline broke", extra=user_extra(request))
