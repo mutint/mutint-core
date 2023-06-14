e9292311b9d602a3852a2b405568ccd951d4fce6
wumuyao1996 <wumuyao1996@gmail.com>
2023-06-14 11:09:55 -0700

    pipeline

--- pipeline/util.py
+++ pipeline/util.py
@@ -1,10 +1,13 @@
+import json
 import subprocess
 
 
 def get_shared_directories():
-    ls_json_cmd = ['ssh', '-i', '/root/.ssh/aledb', 'muyao@aledb.org', 'rclone', 'lsjson', '--dirs-only',
+    lsjson_cmd = ['ssh', '-i', '/root/.ssh/aledb', 'muyao@aledb.org', 'rclone', 'lsjson', '--dirs-only',
               '--drive-shared-with-me', 'ALE:']
-    return subprocess.run(ls_json_cmd)
+    lsjson_out = json.loads(subprocess.run(lsjson_cmd, capture_output=True, text=True).stdout)
+
+    return lsjson_out
 
 
 def get_files_from_directory(dir):
--- pipeline/views.py
+++ pipeline/views.py
@@ -6,7 +6,6 @@ from common.util import get_user_context
 from django.template import loader
 from logs.aledb_logger import user_extra
 from pipeline.util import get_shared_directories
-import json
 import logging
 
 
@@ -20,7 +19,7 @@ def pipeline(request):
     logger.info("pipeline", extra=user_extra(request))
     context = get_user_context(request.user)
 
-    shared_directories_list = json.loads(get_shared_directories())
+    shared_directories_list = get_shared_directories()
     context.update({"shared_drives": shared_directories_list})
 
     if request.method == "POST":
