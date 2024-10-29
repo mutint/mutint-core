c411bf69e7ef9fa26ae97ad64dac51d3c0b0cb5c
Muyao Bob Wu <muyao@MeowBook.local>
2024-10-29 08:33:30 -0700

    updates

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,6 @@ from django.contrib.auth import views as auth_views
 app_name = 'accounts'
 
 urlpatterns = [
-    re_path(r'^login/$', auth_views.LoginView.as_view(redirect_authenticated_user=True), name='login'),
+    re_path(r'^login/$', auth_views.LoginView.as_view(), {'next_page': '/'}, name='login'),
     re_path(r'^logout/$', auth_views.LogoutView.as_view(), {'next_page': '/'}, name='logout'),
 ]
