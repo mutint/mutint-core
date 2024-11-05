6cf14ee02ccc11a6eccf3430c984eb80a188db38
Muyao Bob Wu <muyao@MeowBook.local>
2024-11-05 03:43:54 -0800

    updates

--- goggles/consumers.py
+++ goggles/consumers.py
@@ -16,6 +16,7 @@ class GogglesConsumer(WebsocketConsumer):
         name = text_data_json['name']
         machine = text_data_json['machine']
         data_id = text_data_json['data_id']
+        sample_name = text_data_json['sample_name']
         self.send(text_data=json.dumps({
-            'message': [machine+db_id, get_experiment_data(machine, db_id), name, data_id, [db_id, name, machine, data_id]]
+            'message': [machine+db_id, get_experiment_data(machine, db_id), sample_name, data_id, [db_id, name, machine, data_id]]
         }, indent=4, sort_keys=True, default=str))
