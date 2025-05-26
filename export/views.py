5f86e885e44577ea6e9eecde58d6a42dd7d9c19b
Zhiwei Li <zhlia@dtu.dk>
2025-05-26 16:11:48 +0200

    change loading CSV and ZIP files

--- export/views.py
+++ export/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.http import StreamingHttpResponse
+from zipstream import ZipStream, ZIP_DEFLATED
 from django.shortcuts import get_object_or_404
 from ale.utils import get_all_user_exps
 from ale.permissions import can_view_project
@@ -8,9 +10,12 @@ import io, csv
 from export.util import get_csv_str
 from logs.aledb_logger import user_extra
 import logging
+import re
 
 logger = logging.getLogger(__name__)
 
+def safe_filename(name):
+    return re.sub(r'[^a-zA-Z0-9_\-]', '_', name)
 
 def export(request):
     logger.info("export", extra = user_extra(request))
@@ -29,24 +34,25 @@ def export(request):
             experiments = get_all_user_exps(request.user)
 
         if mut_type_str and exp_id_str:
-            exp_list = []
-            exp_id_list = exp_id_str.split(',')
-            for exp in experiments:
-                if str(exp.ale_id) in exp_id_list:
-                    exp_list.append(exp)
+            exp_id_set = set(exp_id_str.split(','))
+            exp_list = [exp for exp in experiments if str(exp.ale_id) in exp_id_set]
             if len(exp_list) > 0:
-                zip_data = io.BytesIO()
-                zip_file = ZipFile(zip_data, 'w')
-                for experiment in exp_list:
-                    csv_data = io.StringIO()
-                    writer = csv.writer(csv_data)
-                    writer.writerows(get_csv_str(experiment.ale_id, mut_type_str))
-                    csv_data.seek(0)
-                    zip_file.writestr(experiment.project.name + '_' + experiment.name + '_' + mut_type_str + '.csv', csv_data.read())
-                zip_file.close()
-                response = HttpResponse(zip_data.getvalue(), content_type='application/x-zip-compressed')
+                def generate_zip():
+                    zip_data = ZipStream(compress_type=ZIP_DEFLATED, compress_level=0)
+                    for experiment in exp_list:
+                        csv_buffer = io.BytesIO()
+                        writer = csv.writer(io.TextIOWrapper(csv_buffer, encoding='utf-8', write_through=True))
+                        writer.writerows(get_csv_str(experiment.ale_id, mut_type_str))
+                        filename = f"{safe_filename(experiment.project.name)}_{safe_filename(experiment.name)}_{mut_type_str}.csv"
+                        csv_bytes = csv_buffer.getvalue()
+                        zip_data.add(csv_bytes, filename)
+                        logger.info(f"Added {filename} to zip", extra=user_extra(request))
+                    yield from zip_data
+                response = StreamingHttpResponse(generate_zip(), content_type='application/zip')
                 response['Content-Disposition'] = 'attachment; filename="download.zip"'
                 return response
-        return HttpResponse(status=403)
     except Exception :
         logger.exception("export broke", extra = user_extra(request))
+        return HttpResponse("Internal server error during export.", status=500)
+    # Catch any path that doesn't return explicitly
+    return HttpResponse("Invalid export request.", status=400)
